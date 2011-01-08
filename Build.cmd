9de5d6b8c7c865e4017a46d239e126794958ec0e
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2011-01-08 22:42:27 +0000

    Enhance build cmd

--- Build.cmd
+++ Build.cmd
@@ -1,2 +1,12 @@
 @echo off
+if "%1"=="PublishPackages" goto publish
+
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe buildscripts\build.proj /p:BuildSilverlight=false %*
+goto end
+
+:publish
+powershell.exe -noprofile buildscripts\publish-nuget-packages.ps1
+
+goto end
+
+:end
