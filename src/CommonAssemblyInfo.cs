aa28b6cc3c19d8b71d02c151d7d7a958a142de9c
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2012-08-17 22:02:16 +0100

    Ensure that the build script signs the resource assemblies for the signed package.

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -26,8 +26,8 @@ using System.Security;
 [assembly : AssemblyProduct("FluentValidation")]
 [assembly : AssemblyCopyright("Copyright (c) Jeremy Skinner 2008-2012")]
 [assembly : ComVisible(false)]
-[assembly : AssemblyVersion("3.4.2.0")]
-[assembly : AssemblyFileVersion("3.4.2.0")]
+[assembly : AssemblyVersion("3.4.3.0")]
+[assembly : AssemblyFileVersion("3.4.3.0")]
 [assembly: CLSCompliant(true)]
 #if !SILVERLIGHT
 [assembly: AllowPartiallyTrustedCallers]
