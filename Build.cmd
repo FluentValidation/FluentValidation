aee0c32017025d4ff9eea22cbfab86880fa6697d
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2013-10-22 12:02:48 +0100

    Move to NuGet for MSBuild community tasks

--- Build.cmd
+++ Build.cmd
@@ -1,11 +1,11 @@
 @echo off
 if "%1"=="PublishPackages" goto publish
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe buildscripts\build.proj /p:BuildSilverlight=false %*
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe .build\build.proj /p:BuildSilverlight=false %*
 goto end
 
 :publish
-powershell.exe -noprofile buildscripts\publish-nuget-packages.ps1
+powershell.exe -noprofile .build\publish-nuget-packages.ps1
 
 goto end
 
