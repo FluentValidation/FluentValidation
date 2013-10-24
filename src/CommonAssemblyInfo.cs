190d85302165adba14b01723a499ba48bfa527ce
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2013-10-24 16:25:09 +0100

    Bump version

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Security;
 #if !PORTABLE
 [assembly : ComVisible(false)]
 #endif
-[assembly : AssemblyVersion("5.0.0.0")]
-[assembly : AssemblyFileVersion("5.0.0.0")]
+[assembly : AssemblyVersion("5.0.0.1")]
+[assembly : AssemblyFileVersion("5.0.0.1")]
 [assembly: CLSCompliant(true)]
 [assembly: System.Resources.NeutralResourcesLanguage("en")]
