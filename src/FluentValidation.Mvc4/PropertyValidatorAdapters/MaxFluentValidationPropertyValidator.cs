08d13549170a849ab14fd638f1f5332b820e9196
matteobaglini <matteo.baglini@gmail.com>
2014-08-04 19:07:55 +0200

    Support MVC4/5 client-side validation for LessThanOrEqualValidator

---
+++ src/FluentValidation.Mvc4/PropertyValidatorAdapters/MaxFluentValidationPropertyValidator.cs
@@ -0,0 +1,27 @@
+﻿namespace FluentValidation.Mvc {
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+    using Internal;
+    using Validators;
+
+    internal class MaxFluentValidationPropertyValidator : FluentValidationPropertyValidator {
+        LessThanOrEqualValidator RangeValidator
+        {
+            get { return (LessThanOrEqualValidator)Validator; }
+        }
+		
+        public MaxFluentValidationPropertyValidator(ModelMetadata metadata, ControllerContext controllerContext, PropertyRule propertyDescription, IPropertyValidator validator) : base(metadata, controllerContext, propertyDescription, validator) {
+            ShouldValidate=false;
+        }
+
+        public override IEnumerable<ModelClientValidationRule> GetClientValidationRules() {
+            if (!ShouldGenerateClientSideRules()) yield break;
+
+            var formatter = new MessageFormatter()
+                .AppendPropertyName(Rule.GetDisplayName())
+                .AppendArgument("ComparisonValue", RangeValidator.ValueToCompare);
+            var message = formatter.BuildMessage(RangeValidator.ErrorMessageSource.GetString());
+            yield return new ModelClientValidationRangeRule(message, null, RangeValidator.ValueToCompare);
+        }
+    }
+}
