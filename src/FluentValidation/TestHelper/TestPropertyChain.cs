8455d52ce2bce058991e208289d74a62ab13315d
smurtdev <smurtdev@users.noreply.github.com>
2015-06-20 21:41:41 +0300

    #84 Fluent tests. Methods renaming...
    #86 Extended the test helper framework so that assertions can be made
    
    #86 Extended the test helper framework so that assertions can be made
    
    revert unintentional changes
    
    #86 Extended the test helper framework so that assertions can be made aga…
    
    add an unit test to show capabilities of fluent testing approach
    
    Hide ability to double side test property configuration to avoid confusions
    
    When usage simplifying
    
    + license info
    
    recreate removed license information
    
    integrate new validation test methods with existing code
    
    more fluent tests for validations

---
+++ src/FluentValidation/TestHelper/TestPropertyChain.cs
@@ -0,0 +1,32 @@
+namespace FluentValidation.TestHelper
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Reflection;
+    using FluentValidation;
+    using Results;
+
+    internal class TestPropertyChain<TValue, TValue1> : ITestPropertyChain<TValue>{
+        private readonly IValidationResultTester validationResultTester;
+        private readonly IEnumerable<MemberInfo> properties;
+
+        public TestPropertyChain(IValidationResultTester validationResultTester, IEnumerable<MemberInfo> properties = null){
+            this.validationResultTester = validationResultTester;
+            this.properties = properties ?? Enumerable.Empty<MemberInfo>();
+        }
+
+        public ITestPropertyChain<TValue2> Property<TValue2>(Expression<Func<TValue, TValue2>> memberAccessor){
+            return new TestPropertyChain<TValue2, TValue1>(validationResultTester, properties.Concat(new[] { ((MemberAccessor<TValue, TValue2>)memberAccessor).Member }));
+        }
+
+        public IEnumerable<ValidationFailure> ShouldHaveValidationError(){
+           return validationResultTester.ShouldHaveValidationError(properties);
+        }
+
+        public void ShouldNotHaveValidationError(){
+            validationResultTester.ShouldNotHaveValidationError(properties);
+        }
+    }
+}
