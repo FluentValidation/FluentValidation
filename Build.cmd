f108d9235bc54abed44cda9905dd4031e71b82a5
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2015-12-22 11:37:16 +0000

    Main build file should still use vs2013 version of msbuild for now

--- Build.cmd
+++ Build.cmd
@@ -3,11 +3,10 @@ if "%1"=="PublishPackages" goto publish
 
 
 if exist %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe set MSBUILD=%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
-if exist "%ProgramFiles%\MSBuild\14.0\bin\msbuild.exe" set MSBUILD=%ProgramFiles%\MSBuild\14.0\bin\msbuild.exe
-if exist "%ProgramFiles(x86)%\MSBuild\14.0\bin\msbuild.exe" set MSBUILD=%ProgramFiles(x86)%\MSBuild\14.0\bin\msbuild.exe
+::if exist "%ProgramFiles%\MSBuild\14.0\bin\msbuild.exe" set MSBUILD=%ProgramFiles%\MSBuild\14.0\bin\msbuild.exe
+::if exist "%ProgramFiles(x86)%\MSBuild\14.0\bin\msbuild.exe" set MSBUILD=%ProgramFiles(x86)%\MSBuild\14.0\bin\msbuild.exe
 
-
-"%MSBUILD%" .build\build.proj /p:BuildSilverlight=false %*
+"%MSBUILD%" .build\build.proj /p:BuildSilverlight=false /p:vs2015=false %*
 goto end
 
 :publish
