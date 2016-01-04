bfb2fc28b2dd557fa6f0da4a83eeb76b6b20edee
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2016-01-04 10:11:37 +0000

    Additional ctor for validatonexception

--- src/FluentValidation/ValidationException.cs
+++ src/FluentValidation/ValidationException.cs
@@ -25,6 +25,10 @@ namespace FluentValidation {
 	public class ValidationException : Exception {
 		public IEnumerable<ValidationFailure> Errors { get; private set; }
 
+	    public ValidationException(string message) : this(message, Enumerable.Empty<ValidationFailure>()) {
+	        
+	    }
+
 		public ValidationException(string message, IEnumerable<ValidationFailure> errors) : base(message) {
 			Errors = errors;
 		}
