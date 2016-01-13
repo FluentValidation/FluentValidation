a842db0277bbf2983b27542d3bad09b03b20de50
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2016-01-13 15:11:00 +0000

    Remove VS2013 solution

--- Build.cmd
+++ Build.cmd
@@ -3,10 +3,10 @@ if "%1"=="PublishPackages" goto publish
 
 
 if exist %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe set MSBUILD=%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
-::if exist "%ProgramFiles%\MSBuild\14.0\bin\msbuild.exe" set MSBUILD=%ProgramFiles%\MSBuild\14.0\bin\msbuild.exe
-::if exist "%ProgramFiles(x86)%\MSBuild\14.0\bin\msbuild.exe" set MSBUILD=%ProgramFiles(x86)%\MSBuild\14.0\bin\msbuild.exe
+if exist "%ProgramFiles%\MSBuild\14.0\bin\msbuild.exe" set MSBUILD=%ProgramFiles%\MSBuild\14.0\bin\msbuild.exe
+if exist "%ProgramFiles(x86)%\MSBuild\14.0\bin\msbuild.exe" set MSBUILD=%ProgramFiles(x86)%\MSBuild\14.0\bin\msbuild.exe
 
-"%MSBUILD%" .build\build.proj /p:BuildSilverlight=false /p:vs2015=false %*
+"%MSBUILD%" .build\build.proj %*
 goto end
 
 :publish
