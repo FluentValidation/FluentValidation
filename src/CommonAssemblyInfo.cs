0000eb187bc6b6a67496dcc0f658d19e7951d119
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2016-02-05 15:15:24 +0000

    Bump version

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly : AssemblyVersion("6.1.0.0")]
-[assembly : AssemblyFileVersion("6.1.0.0")]
+[assembly : AssemblyVersion("6.2.0.0")]
+[assembly : AssemblyFileVersion("6.2.0.0")]
