9c335cfd33bc2b966ba74d168a0c9bea445c613a
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2016-02-26 09:49:07 +0000

    Formatting, and fix the message localization for EnumValidator.

--- src/FluentValidation.Tests/EnumValidatorTests.cs
+++ src/FluentValidation.Tests/EnumValidatorTests.cs
@@ -1,12 +1,14 @@
 namespace FluentValidation.Tests
 {
-    using System.Linq;
-    using Xunit;
+	using System.Linq;
+	using Xunit;
 
-    public class EnumValidatorTests {
+	public class EnumValidatorTests
+	{
 		TestValidator validator;
 
-		public EnumValidatorTests() {
+		public EnumValidatorTests()
+		{
 			CultureScope.SetDefaultCulture();
 
 			validator = new TestValidator {
@@ -15,62 +17,65 @@ namespace FluentValidation.Tests
 		}
 
 		[Fact]
-		public void IsValidTests() {
+		public void IsValidTests()
+		{
 			validator.Validate(new Person { Gender = EnumGender.Female }).IsValid.ShouldBeTrue();  // Simplest valid value
 			validator.Validate(new Person { Gender = EnumGender.Male }).IsValid.ShouldBeTrue();    // Other valid value
-            validator.Validate(new Person { Gender = (EnumGender)1 }).IsValid.ShouldBeTrue();      // Casting with valid value
-        }
-
-        [Fact]
-        public void When_the_enum_is_not_initialized_with_valid_value_then_the_validator_should_fail() {
-            var result = validator.Validate(new Person());                                         // Default value 0 is not defined in Enum
-            result.IsValid.ShouldBeFalse();
-        }
-
-        [Fact]
-        public void When_the_enum_is_initialized_with_invalid_value_then_the_validator_should_fail()
-        {
-            var result = validator.Validate(new Person { Gender = (EnumGender)3 });                // 3 in not defined in Enum
-            result.IsValid.ShouldBeFalse();
-        }
-
-
-        [Fact]
-		public void When_validation_fails_the_default_error_should_be_set() {
-			var result = validator.Validate(new Person());
-			result.Errors.Single().ErrorMessage.ShouldEqual("Property Gender it not a valid enum value.");
+			validator.Validate(new Person { Gender = (EnumGender)1 }).IsValid.ShouldBeTrue();      // Casting with valid value
 		}
 
-        [Fact]
-        public void Nullable_enum_valid_when_property_value_is_null()
-        {
-            var validator = new InlineValidator<Foo>();
-             validator.RuleFor(x => x.Gender).IsInEnum();
-            var result = validator.Validate(new Foo());
-            result.IsValid.ShouldBeTrue();
-        }
+		[Fact]
+		public void When_the_enum_is_not_initialized_with_valid_value_then_the_validator_should_fail()
+		{
+			var result = validator.Validate(new Person());                                         // Default value 0 is not defined in Enum
+			result.IsValid.ShouldBeFalse();
+		}
 
-        [Fact]
-        public void Nullable_enum_valid_when_valid_value_specified()
-        {
-            var validator = new InlineValidator<Foo>();
-            validator.RuleFor(x => x.Gender).IsInEnum();
-            var result = validator.Validate(new Foo() { Gender = EnumGender.Male });
-            result.IsValid.ShouldBeTrue();
-        }
+		[Fact]
+		public void When_the_enum_is_initialized_with_invalid_value_then_the_validator_should_fail()
+		{
+			var result = validator.Validate(new Person { Gender = (EnumGender)3 });                // 3 in not defined in Enum
+			result.IsValid.ShouldBeFalse();
+		}
 
-        [Fact]
-        public void Nullable_enum_invalid_on_wrong_value()
-        {
-            var validator = new InlineValidator<Foo>();
-            validator.RuleFor(x => x.Gender).IsInEnum();
-            var result = validator.Validate(new Foo() { Gender = (EnumGender)42 });
-            result.IsValid.ShouldBeFalse();
-        }
 
-        public class Foo
-        {
-            public EnumGender? Gender { get; set; }
-        }
+		[Fact]
+		public void When_validation_fails_the_default_error_should_be_set()
+		{
+			var result = validator.Validate(new Person());
+			result.Errors.Single().ErrorMessage.ShouldEqual("The value for 'Gender' is invalid.");
+		}
+
+		[Fact]
+		public void Nullable_enum_valid_when_property_value_is_null()
+		{
+			var validator = new InlineValidator<Foo>();
+			validator.RuleFor(x => x.Gender).IsInEnum();
+			var result = validator.Validate(new Foo());
+			result.IsValid.ShouldBeTrue();
+		}
+
+		[Fact]
+		public void Nullable_enum_valid_when_value_specified()
+		{
+			var validator = new InlineValidator<Foo>();
+			validator.RuleFor(x => x.Gender).IsInEnum();
+			var result = validator.Validate(new Foo() { Gender = EnumGender.Male });
+			result.IsValid.ShouldBeTrue();
+		}
+
+		[Fact]
+		public void Nullable_enum_invalid_when_bad_value_specified()
+		{
+			var validator = new InlineValidator<Foo>();
+			validator.RuleFor(x => x.Gender).IsInEnum();
+			var result = validator.Validate(new Foo() { Gender = (EnumGender)42 });
+			result.IsValid.ShouldBeFalse();
+		}
+
+		public class Foo
+		{
+			public EnumGender? Gender { get; set; }
+		}
 	}
 }
--- src/FluentValidation/Validators/EnumValidator.cs
+++ src/FluentValidation/Validators/EnumValidator.cs
@@ -1,20 +1,21 @@
 ﻿namespace FluentValidation.Validators {
 	using System;
 	using FluentValidation.Internal;
+	using Resources;
 
 	public class EnumValidator : PropertyValidator {
 		private readonly Type _enumType;
 
-		public EnumValidator(Type enumType) : base("Property {PropertyName} it not a valid enum value.") {
+		public EnumValidator(Type enumType) : base(() => Messages.enum_error) {
 			this._enumType = enumType;
 		}
 
 		protected override bool IsValid(PropertyValidatorContext context) {
-            if (context.PropertyValue == null) return true;
+			if (context.PropertyValue == null) return true;
 
-            Type enumType = Nullable.GetUnderlyingType(_enumType) ?? _enumType;
+			Type enumType = Nullable.GetUnderlyingType(_enumType) ?? _enumType;
 
-            if (!enumType.IsEnum()) return false;
+			if (!enumType.IsEnum()) return false;
 			return Enum.IsDefined(enumType, context.PropertyValue);
 		}
 	}
