f90fc557d89bf27f41c4c07228be7cc4c2dc7eae
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2016-05-13 10:43:29 +0100

    Reformat

--- src/FluentValidation/DefaultValidatorExtensions.cs
+++ src/FluentValidation/DefaultValidatorExtensions.cs
@@ -876,35 +876,33 @@ namespace FluentValidation {
 			return validator.ValidateAsync(context);
 		}
 
-        /// <summary>
-        /// Performs validation and then throws an exception if validation fails.
-        /// </summary>
-        /// <param name="validator">The validator this method is extending.</param>
-        /// <param name="instance">The instance of the type we are validating.</param>
-        /// <param name="ruleSet">Optional: a ruleset when need to validate against.</param>
-		public static void ValidateAndThrow<T>(this IValidator<T> validator, T instance, string ruleSet = null)
-        {
-            var result = validator.Validate(instance, ruleSet: ruleSet);
+		/// <summary>
+		/// Performs validation and then throws an exception if validation fails.
+		/// </summary>
+		/// <param name="validator">The validator this method is extending.</param>
+		/// <param name="instance">The instance of the type we are validating.</param>
+		/// <param name="ruleSet">Optional: a ruleset when need to validate against.</param>
+		public static void ValidateAndThrow<T>(this IValidator<T> validator, T instance, string ruleSet = null) {
+			var result = validator.Validate(instance, ruleSet: ruleSet);
 
-            if (!result.IsValid)
-            {
-                throw new ValidationException(result.Errors);
-            }
-        }
+			if (!result.IsValid) {
+				throw new ValidationException(result.Errors);
+			}
+		}
 
-	    /// <summary>
-	    /// Performs validation asynchronously and then throws an exception if validation fails.
-	    /// </summary>
-	    /// <param name="validator">The validator this method is extending.</param>
-	    /// <param name="instance">The instance of the type we are validating.</param>
-	    /// <param name="ruleSet">Optional: a ruleset when need to validate against.</param>
-	    public static Task ValidateAndThrowAsync<T>(this IValidator<T> validator, T instance, string ruleSet = null) {
-	        return validator
-	            .ValidateAsync(instance, ruleSet: ruleSet)
-	            .Then(r => r.IsValid
-	                ? TaskHelpers.Completed()
-	                : TaskHelpers.FromError(new ValidationException(r.Errors)));
-	    }
+		/// <summary>
+		/// Performs validation asynchronously and then throws an exception if validation fails.
+		/// </summary>
+		/// <param name="validator">The validator this method is extending.</param>
+		/// <param name="instance">The instance of the type we are validating.</param>
+		/// <param name="ruleSet">Optional: a ruleset when need to validate against.</param>
+		public static Task ValidateAndThrowAsync<T>(this IValidator<T> validator, T instance, string ruleSet = null) {
+			return validator
+				.ValidateAsync(instance, ruleSet: ruleSet)
+				.Then(r => r.IsValid
+					? TaskHelpers.Completed()
+					: TaskHelpers.FromError(new ValidationException(r.Errors)));
+		}
 
 	    /// <summary>
 		/// Defines an 'inclusive between' validator on the current rule builder, but only for properties of types that implement IComparable.
