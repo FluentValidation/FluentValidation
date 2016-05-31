28ad4810cd89cdfd7f637a2dfa6db94e7ec9f58f
Jeremy Meng <yumeng@microsoft.com>
2016-05-31 11:26:47 -0700

    Specify dependencies for other supported TFM's.
    There's an issue when installing the package for .NET v4.5 project, the netstandard1.0 dependencies are unnecessarily pulled down.  In this change the exact dependencies are specified for each target framework so that correct set of dependencies will be installed when installing the FluentValidation package.

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly : AssemblyVersion("6.3.3.0")]
-[assembly : AssemblyFileVersion("6.3.3.0")]
+[assembly : AssemblyVersion("6.3.4.0")]
+[assembly : AssemblyFileVersion("6.3.4.0")]
