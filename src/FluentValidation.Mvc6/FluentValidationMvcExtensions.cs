355185121e72400b2ff26f3b40774eb60005d948
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2016-06-24 17:39:30 +0100

    Initial work on mvc6 support

---
+++ src/FluentValidation.Mvc6/FluentValidationMvcExtensions.cs
@@ -0,0 +1,47 @@
+﻿namespace FluentValidation.Mvc {
+	using System;
+	using System.Linq;
+	using System.Reflection;
+	using Microsoft.AspNetCore.Mvc.ModelBinding;
+	using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+	using Microsoft.Extensions.DependencyInjection;
+
+	public static class FluentValidationMvcExtensions {
+		/// <summary>
+		///     Adds Fluent Validation services to the specified
+		///     <see cref="T:Microsoft.Extensions.DependencyInjection.IMvcBuilder" />.
+		/// </summary>
+		/// <returns>
+		///     An <see cref="T:Microsoft.Extensions.DependencyInjection.IMvcBuilder" /> that can be used to further configure the
+		///     MVC services.
+		/// </returns>
+		public static IMvcBuilder AddFluentValidation(this IMvcBuilder mvcBuilder) {
+			// add all IValidator to MVC's service provider
+
+			//TODO: JS Need to check if this works
+			var validators =
+				typeof(FluentValidationObjectModelValidator)
+					.GetTypeInfo().Assembly
+					.GetTypes()
+					.Where(t => typeof(IValidator).IsAssignableFrom(t));
+
+			foreach (var validator in validators) {
+				mvcBuilder.Services.AddTransient(validator);
+			}
+
+			// add the fluent validation object model validator
+
+			mvcBuilder.Services.Add(ServiceDescriptor.Transient<IObjectModelValidator>(serviceProvider =>
+				new FluentValidationObjectModelValidator(
+					serviceProvider.GetRequiredService<IModelMetadataProvider>(),
+					new FluentValidationValidatorFactory(serviceProvider.GetRequiredService<IServiceProvider>()))));
+
+			// clear all model validation providers since fluent validation will be handling everything
+
+			mvcBuilder.AddMvcOptions(
+				options => { options.ModelValidatorProviders.Clear(); });
+
+			return mvcBuilder;
+		}
+	}
+}
---
+++ src/FluentValidation.Mvc6/FluentValidationObjectModelValidator.cs
@@ -0,0 +1,64 @@
+﻿namespace FluentValidation.Mvc {
+	using System;
+	using System.Linq;
+	using Microsoft.AspNetCore.Mvc;
+	using Microsoft.AspNetCore.Mvc.ModelBinding;
+	using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+	//TODO: Need support for CustomizeValidatorAttribute and client-side
+
+	public class FluentValidationObjectModelValidator : IObjectModelValidator {
+		private readonly IValidatorFactory _validatorFactory;
+
+		/// <summary>
+		///     Initializes a new instance of <see cref="FluentValidationObjectModelValidator" />.
+		/// </summary>
+		public FluentValidationObjectModelValidator(IModelMetadataProvider modelMetadataProvider,
+			IValidatorFactory validatorFactory) {
+			if (modelMetadataProvider == null) {
+				throw new ArgumentNullException(nameof(modelMetadataProvider));
+			}
+
+			_validatorFactory = validatorFactory;
+		}
+
+		public void Validate(ActionContext actionContext, IModelValidatorProvider validatorProvider,
+			ValidationStateDictionary validationState, string prefix, object model) {
+			if (actionContext == null) {
+				throw new ArgumentNullException(nameof(actionContext));
+			}
+
+			// would model ever be null ??
+
+			if (model == null) {
+				return;
+			}
+
+			// get our IValidator
+
+			var validator = _validatorFactory.GetValidator(model.GetType());
+
+			if (validator == null) {
+				return;
+			}
+
+			foreach (var value in actionContext.ModelState.Values
+				.Where(v => v.ValidationState == ModelValidationState.Unvalidated)) {
+				// Set all unvalidated states to valid. If we end up adding an error below then that properties state
+				// will become ModelValidationState.Invalid and will set ModelState.IsValid to false
+
+				value.ValidationState = ModelValidationState.Valid;
+			}
+
+			// validate the model using Fluent Validation rules
+
+			var result = validator.Validate(model);
+
+			// add all our model errors to the modelstate
+
+			foreach (var modelError in result.Errors) {
+				actionContext.ModelState.AddModelError(modelError.PropertyName, modelError.ErrorMessage);
+			}
+		}
+	}
+}
