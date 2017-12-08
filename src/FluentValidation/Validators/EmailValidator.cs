ebe372044ac0451701cf71069199f5d2da75a3c4
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2017-12-08 11:56:19 +0000

    CVE-2015-2526

--- src/FluentValidation/Validators/EmailValidator.cs
+++ src/FluentValidation/Validators/EmailValidator.cs
@@ -31,7 +31,7 @@ namespace FluentValidation.Validators {
 		const string expression = @"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-||_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+([a-z]+|\d|-|\.{0,1}|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])?([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$";
 
 		public EmailValidator() : base(new LanguageStringSource(nameof(EmailValidator))) {
-			regex = new Regex(expression, RegexOptions.IgnoreCase);
+			regex = CreateRegEx();
 		}
 
 
@@ -45,8 +45,31 @@ namespace FluentValidation.Validators {
 			return true;
 		}
 
-		public string Expression {
-			get { return expression; }
+		public string Expression => expression;
+
+		private static Regex CreateRegEx()
+		{
+			// Workaround for CVE-2015-2526
+			// If no REGEX_DEFAULT_MATCH_TIMEOUT is specified in the AppDomain, default to 2 seconds. 
+			// if we're on Netstandard 1.0 we don't have access to AppDomain, so just always use 2 second timeout there. 
+
+#if NETSTANDARD1_0
+			return new Regex(expression, RegexOptions.IgnoreCase, TimeSpan.FromSeconds(2.0));
+#else
+			try
+			{
+				if (AppDomain.CurrentDomain.GetData("REGEX_DEFAULT_MATCH_TIMEOUT") == null) {
+					return new Regex(expression, RegexOptions.IgnoreCase, TimeSpan.FromSeconds(2.0));
+				}
+			}
+			catch
+			{
+			}
+
+		
+	return new Regex(expression, RegexOptions.IgnoreCase);
+#endif
+
 		}
 	}
 
