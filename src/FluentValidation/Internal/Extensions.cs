30233c822aca56dd508be34c988bfc16c1d64b91
Ricardo Alves <rasf19@hotmail.com>
2018-04-12 15:07:07 -0300

    braces adjustments

--- src/FluentValidation/Internal/Extensions.cs
+++ src/FluentValidation/Internal/Extensions.cs
@@ -16,8 +16,7 @@
 // The latest version of this file can be found at https://github.com/jeremyskinner/FluentValidation
 #endregion
 
-namespace FluentValidation.Internal
-{
+namespace FluentValidation.Internal {
 	using System;
 	using System.Collections.Generic;
 	using System.Linq.Expressions;
@@ -30,25 +29,19 @@ namespace FluentValidation.Internal
 	/// <summary>
 	/// Useful extensions
 	/// </summary>
-	public static class Extensions
-	{
-		internal static void Guard(this object obj, string message, string paramName)
-		{
-			if (obj == null)
-			{
+	public static class Extensions {
+		internal static void Guard(this object obj, string message, string paramName) {
+			if (obj == null) {
 				throw new ArgumentNullException(paramName, message);
 			}
 		}
 
-		internal static void Guard(this string str, string message, string paramName)
-		{
-			if (str == null)
-			{
+		internal static void Guard(this string str, string message, string paramName) {
+			if (str == null) {
 				throw new ArgumentNullException(paramName, message);
 			}
 
-			if (string.IsNullOrEmpty(str))
-			{
+			if (string.IsNullOrEmpty(str)) {
 				throw new ArgumentException(message, paramName);
 			}
 		}
@@ -58,20 +51,17 @@ namespace FluentValidation.Internal
 		/// </summary>
 		/// <param name="expression"></param>
 		/// <returns></returns>
-		public static bool IsParameterExpression(this LambdaExpression expression)
-		{
+		public static bool IsParameterExpression(this LambdaExpression expression) {
 			return expression.Body.NodeType == ExpressionType.Parameter;
 		}
 
 		/// <summary>
 		/// Gets a MemberInfo from a member expression.
 		/// </summary>
-		public static MemberInfo GetMember(this LambdaExpression expression)
-		{
+		public static MemberInfo GetMember(this LambdaExpression expression) {
 			var memberExp = RemoveUnary(expression.Body) as MemberExpression;
 
-			if (memberExp == null)
-			{
+			if (memberExp == null) {
 				return null;
 			}
 
@@ -81,34 +71,27 @@ namespace FluentValidation.Internal
 		/// <summary>
 		/// Gets a MemberInfo from a member expression.
 		/// </summary>
-		public static MemberInfo GetMember<T, TProperty>(this Expression<Func<T, TProperty>> expression)
-		{
+		public static MemberInfo GetMember<T, TProperty>(this Expression<Func<T, TProperty>> expression) {
 			var memberExp = RemoveUnary(expression.Body) as MemberExpression;
 
-			if (memberExp == null)
-			{
+			if (memberExp == null) {
 				return null;
 			}
 
 			Expression currentExpr = memberExp.Expression;
 
 			// Unwind the expression to get the root object that the expression acts upon. 
-			while (true)
-			{
+			while (true) {
 				currentExpr = RemoveUnary(currentExpr);
 
-				if (currentExpr != null && currentExpr.NodeType == ExpressionType.MemberAccess)
-				{
+				if (currentExpr != null && currentExpr.NodeType == ExpressionType.MemberAccess) {
 					currentExpr = ((MemberExpression)currentExpr).Expression;
-				}
-				else
-				{
+				} else {
 					break;
 				}
 			}
 
-			if (currentExpr == null || currentExpr.NodeType != ExpressionType.Parameter)
-			{
+			if (currentExpr == null || currentExpr.NodeType != ExpressionType.Parameter) {
 				return null; // We don't care if we're not acting upon the model instance. 
 			}
 
@@ -116,10 +99,8 @@ namespace FluentValidation.Internal
 		}
 
 
-		private static Expression RemoveUnary(Expression toUnwrap)
-		{
-			if (toUnwrap is UnaryExpression)
-			{
+		private static Expression RemoveUnary(Expression toUnwrap) {
+			if (toUnwrap is UnaryExpression) {
 				return ((UnaryExpression)toUnwrap).Operand;
 			}
 
@@ -129,18 +110,15 @@ namespace FluentValidation.Internal
 		/// <summary>
 		/// Splits pascal case, so "FooBar" would become "Foo Bar"
 		/// </summary>
-		public static string SplitPascalCase(this string input)
-		{
+		public static string SplitPascalCase(this string input) {
 			if (string.IsNullOrEmpty(input))
 				return input;
 
 			var retVal = new StringBuilder(input.Length + 5);
 
-			for (int i = 0; i < input.Length; ++i)
-			{
+			for (int i = 0; i < input.Length; ++i) {
 				var currentChar = input[i];
-				if (char.IsUpper(currentChar))
-				{
+				if (char.IsUpper(currentChar)) {
 					if ((i > 1 && !char.IsUpper(input[i - 1]))
 							|| (i + 1 < input.Length && !char.IsUpper(input[i + 1])))
 						retVal.Append(' ');
@@ -159,65 +137,53 @@ namespace FluentValidation.Internal
 		/// <typeparam name="TProperty">Type of property being validated</typeparam>
 		/// <param name="valueToCompare">The value being compared</param>
 		/// <returns></returns>
-		internal static Expression<Func<T, TProperty>> GetConstantExpresionFromConstant<T, TProperty>(TProperty valueToCompare)
-		{
+		internal static Expression<Func<T, TProperty>> GetConstantExpresionFromConstant<T, TProperty>(TProperty valueToCompare) {
 			Expression constant = Expression.Constant(valueToCompare, typeof(TProperty));
 			ParameterExpression parameter = Expression.Parameter(typeof(T), "t");
 			return Expression.Lambda<Func<T, TProperty>>(constant, parameter);
 		}
 
-		internal static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
-		{
-			foreach (var item in source)
-			{
+		internal static void ForEach<T>(this IEnumerable<T> source, Action<T> action) {
+			foreach (var item in source) {
 				action(item);
 			}
 		}
 
 #pragma warning disable 1591
-		public static Func<object, object> CoerceToNonGeneric<T, TProperty>(this Func<T, TProperty> func)
-		{
+		public static Func<object, object> CoerceToNonGeneric<T, TProperty>(this Func<T, TProperty> func) {
 			return x => func((T)x);
 		}
 
-		public static Func<object, bool> CoerceToNonGeneric<T>(this Func<T, bool> func)
-		{
+		public static Func<object, bool> CoerceToNonGeneric<T>(this Func<T, bool> func) {
 			return x => func((T)x);
 		}
 
-		public static Func<object, CancellationToken, Task<bool>> CoerceToNonGeneric<T>(this Func<T, CancellationToken, Task<bool>> func)
-		{
+		public static Func<object, CancellationToken, Task<bool>> CoerceToNonGeneric<T>(this Func<T, CancellationToken, Task<bool>> func) {
 			return (x, ct) => func((T)x, ct);
 		}
 
 
-		public static Func<object, Task<bool>> CoerceToNonGeneric<T>(this Func<T, Task<bool>> func)
-		{
+		public static Func<object, Task<bool>> CoerceToNonGeneric<T>(this Func<T, Task<bool>> func) {
 			return x => func((T)x);
 		}
 
-		public static Func<object, int> CoerceToNonGeneric<T>(this Func<T, int> func)
-		{
+		public static Func<object, int> CoerceToNonGeneric<T>(this Func<T, int> func) {
 			return x => func((T)x);
 		}
 
-		public static Func<object, long> CoerceToNonGeneric<T>(this Func<T, long> func)
-		{
+		public static Func<object, long> CoerceToNonGeneric<T>(this Func<T, long> func) {
 			return x => func((T)x);
 		}
 
-		public static Func<object, string> CoerceToNonGeneric<T>(this Func<T, string> func)
-		{
+		public static Func<object, string> CoerceToNonGeneric<T>(this Func<T, string> func) {
 			return x => func((T)x);
 		}
 
-		public static Func<object, System.Text.RegularExpressions.Regex> CoerceToNonGeneric<T>(this Func<T, System.Text.RegularExpressions.Regex> func)
-		{
+		public static Func<object, System.Text.RegularExpressions.Regex> CoerceToNonGeneric<T>(this Func<T, System.Text.RegularExpressions.Regex> func) {
 			return x => func((T)x);
 		}
 
-		public static Action<object> CoerceToNonGeneric<T>(this Action<T> action)
-		{
+		public static Action<object> CoerceToNonGeneric<T>(this Action<T> action) {
 			return x => action((T)x);
 		}
 #pragma warning restore 1591
