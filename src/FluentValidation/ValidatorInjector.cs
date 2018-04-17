1bfe940aa47eeef325e64e48229b2b15a920b3c7
Ricardo Alves <rasf19@hotmail.com>
2018-04-17 08:04:42 -0300

    Added ValidatorInjector, based in Issue #590 - Register open generics in ASP.NET Core

---
+++ src/FluentValidation/ValidatorInjector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FluentValidation
+{
+	public class ValidatorInjector<TValidator> : IValidatorInjector<TValidator>
+		where TValidator : IValidator, new()
+	{
+		private TValidator validator;
+
+		public TValidator Validator => validator;
+
+		public ValidatorInjector()
+		{
+			validator = new TValidator();
+		}
+	}
+}
