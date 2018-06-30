2644d12189645c2830b8f973d1814ed65f963acd
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2018-06-30 10:59:09 +0100

    Async cascade mode tests

--- src/FluentValidation.Tests/CascadingFailuresTester.cs
+++ src/FluentValidation.Tests/CascadingFailuresTester.cs
@@ -18,6 +18,7 @@
 
 namespace FluentValidation.Tests {
     using System;
+    using System.Threading.Tasks;
     using Xunit;
 
 
@@ -115,6 +116,93 @@ namespace FluentValidation.Tests {
 			validator.RuleFor(x => x.Surname).NotNull().Equal("Foo");
 			validator.CascadeMode = CascadeMode.StopOnFirstFailure;
 			var results = validator.Validate(new Person());
+			results.Errors.Count.ShouldEqual(1);
+		}
+
+	    
+	    
+	    [Fact]
+		public async Task Validation_continues_on_failure_async() {
+			validator.RuleFor(x => x.Surname).NotNull().Equal("Foo");
+			var results = await validator.ValidateAsync(new Person());
+			results.Errors.Count.ShouldEqual(2);
+		}
+
+		[Fact]
+		public async Task Validation_stops_on_first_failure_async() {
+			ValidatorOptions.CascadeMode = CascadeMode.StopOnFirstFailure;
+
+			validator.RuleFor(x => x.Surname).NotNull().Equal("Foo");
+			var results = await validator.ValidateAsync(new Person());
+			results.Errors.Count.ShouldEqual(1);
+		}
+
+		[Fact]
+		public async Task Validation_continues_on_failure_when_set_to_Stop_globally_and_overriden_at_rule_level_async() {
+			ValidatorOptions.CascadeMode = CascadeMode.StopOnFirstFailure;
+
+			validator.RuleFor(x => x.Surname).Cascade(CascadeMode.Continue).NotNull().Equal("Foo");
+			var results = await validator.ValidateAsync(new Person());
+			results.Errors.Count.ShouldEqual(2);
+		}
+
+		[Fact]
+		public async Task Validation_stops_on_first_Failure_when_set_to_Continue_globally_and_overriden_at_rule_level_async() {
+			ValidatorOptions.CascadeMode = CascadeMode.Continue;
+			validator.RuleFor(x => x.Surname).Cascade(CascadeMode.StopOnFirstFailure).NotNull().Equal("Foo");
+			var results = await validator.ValidateAsync(new Person());
+			results.Errors.Count.ShouldEqual(1);
+		}
+
+		[Fact]
+		public async Task Validation_continues_to_second_validator_when_first_validator_succeeds_and_cascade_set_to_stop_async() {
+			ValidatorOptions.CascadeMode = CascadeMode.StopOnFirstFailure;
+			validator.RuleFor(x => x.Surname).NotNull().Length(2, 10);
+			var result = await validator.ValidateAsync(new Person {Surname = "x"});
+			result.IsValid.ShouldBeFalse();
+		}
+
+		[Fact]
+		public async Task Validation_stops_on_first_failure_when_set_to_StopOnFirstFailure_at_validator_level_async() {
+			validator.CascadeMode = CascadeMode.StopOnFirstFailure;
+
+			validator.RuleFor(x => x.Surname).NotNull().Equal("Foo");
+			var results = await validator.ValidateAsync(new Person());
+			results.Errors.Count.ShouldEqual(1);
+		}
+
+		[Fact]
+		public async Task Validation_continues_when_set_to_Continue_at_validator_level_async() {
+			validator.CascadeMode = CascadeMode.Continue;
+
+			validator.RuleFor(x => x.Surname).NotNull().Equal("Foo");
+			var results = await validator.ValidateAsync(new Person());
+			results.Errors.Count.ShouldEqual(2);
+		}
+
+		[Fact]
+		public async Task Validation_continues_on_failure_when_set_to_StopOnFirstFailure_at_validator_level_and_overriden_at_rule_level_async() {
+			validator.CascadeMode = CascadeMode.StopOnFirstFailure;
+
+			validator.RuleFor(x => x.Surname).Cascade(CascadeMode.Continue).NotNull().Equal("Foo");
+			var results = await validator.ValidateAsync(new Person());
+			results.Errors.Count.ShouldEqual(2);
+		}
+
+		[Fact]
+		public async Task Validation_stops_on_failure_when_set_to_Continue_and_overriden_at_rule_level_async() {
+			validator.CascadeMode = CascadeMode.Continue;
+
+			validator.RuleFor(x => x.Surname).Cascade(CascadeMode.StopOnFirstFailure).NotNull().Equal("Foo");
+			var results = await validator.ValidateAsync(new Person());
+			results.Errors.Count.ShouldEqual(1);
+		}
+
+		[Fact]
+		public async Task Cascade_mode_can_be_set_after_validator_instantiated_async() {
+			validator.RuleFor(x => x.Surname).NotNull().Equal("Foo");
+			validator.CascadeMode = CascadeMode.StopOnFirstFailure;
+			var results = await validator.ValidateAsync(new Person());
 			results.Errors.Count.ShouldEqual(1);
 		}
 
