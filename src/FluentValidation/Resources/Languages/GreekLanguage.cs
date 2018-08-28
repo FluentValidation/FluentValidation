365189abab44586091c30817ca83fa58a8245d99
nsarris <nsarris@gmail.com>
2018-08-28 15:12:06 +0300

    Fixed a 'not' missing in NotNullValidator message

--- src/FluentValidation/Resources/Languages/GreekLanguage.cs
+++ src/FluentValidation/Resources/Languages/GreekLanguage.cs
@@ -33,7 +33,7 @@ namespace FluentValidation.Resources {
 			Translate<LessThanValidator>("Το πεδίο '{PropertyName}' πρέπει να έχει τιμή μικρότερη από '{ComparisonValue}'.");
 			Translate<NotEmptyValidator>("Το πεδίο '{PropertyName}' δεν πρέπει να είναι κενό.");
 			Translate<NotEqualValidator>("Το πεδίο '{PropertyName}' δεν πρέπει να έχει τιμή ίση με '{ComparisonValue}'.");
-			Translate<NotNullValidator>("Το πεδίο '{PropertyName}' πρέπει να είναι κενό.");
+			Translate<NotNullValidator>("Το πεδίο '{PropertyName}' δεν πρέπει να είναι κενό.");
 			Translate<PredicateValidator>("Η ορισμένη συνθήκη δεν ικανοποιήθηκε για το πεδίο '{PropertyName}'.");
 			Translate<AsyncPredicateValidator>("Η ορισμένη συνθήκη δεν ικανοποιήθηκε για το πεδίο '{PropertyName}'.");
 			Translate<RegularExpressionValidator>("Η τιμή του πεδίου '{PropertyName}' δεν έχει αποδεκτή μορφή.");
