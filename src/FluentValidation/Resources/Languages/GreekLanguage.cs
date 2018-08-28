b3c3fc021e56ad730b040e6016fdb7e615fdd27a
nsarris <nsarris@gmail.com>
2018-08-28 12:05:35 +0300

    Update GreekLanguage.cs

--- src/FluentValidation/Resources/Languages/GreekLanguage.cs
+++ src/FluentValidation/Resources/Languages/GreekLanguage.cs
@@ -30,22 +30,22 @@ namespace FluentValidation.Resources {
 			Translate<MinimumLengthValidator>("Το μήκος του πεδίου '{PropertyName}' πρέπει να είναι τουλάχιστον {MinLength} χαρακτήρες. Έχετε καταχωρίσει {TotalLength} χαρακτήρες.");
 			Translate<MaximumLengthValidator>("Το μήκος του πεδίου '{PropertyName}' πρέπει να είναι το πολύ {MaxLength} χαρακτήρες. Έχετε καταχωρίσει {TotalLength} χαρακτήρες.");
 			Translate<LessThanOrEqualValidator>("Το πεδίο '{PropertyName}' πρέπει να έχει τιμή μικρότερη ή ίση με '{ComparisonValue}'.");
-			Translate<LessThanValidator>("'Το πεδίο '{PropertyName}' πρέπει να έχει τιμή μικρότερη από '{ComparisonValue}'.");
-			Translate<NotEmptyValidator>("'Το πεδίο '{PropertyName}' δεν πρέπει να είναι κενό.");
-			Translate<NotEqualValidator>("'Το πεδίο '{PropertyName}' δεν πρέπει να έχει τιμή ίση με '{ComparisonValue}'.");
-			Translate<NotNullValidator>("'Το πεδίο '{PropertyName}' πρέπει να είναι κενό.");
+			Translate<LessThanValidator>("Το πεδίο '{PropertyName}' πρέπει να έχει τιμή μικρότερη από '{ComparisonValue}'.");
+			Translate<NotEmptyValidator>("Το πεδίο '{PropertyName}' δεν πρέπει να είναι κενό.");
+			Translate<NotEqualValidator>("Το πεδίο '{PropertyName}' δεν πρέπει να έχει τιμή ίση με '{ComparisonValue}'.");
+			Translate<NotNullValidator>("Το πεδίο '{PropertyName}' πρέπει να είναι κενό.");
 			Translate<PredicateValidator>("Η ορισμένη συνθήκη δεν ικανοποιήθηκε για το πεδίο '{PropertyName}'.");
 			Translate<AsyncPredicateValidator>("Η ορισμένη συνθήκη δεν ικανοποιήθηκε για το πεδίο '{PropertyName}'.");
 			Translate<RegularExpressionValidator>("Η τιμή του πεδίου '{PropertyName}' δεν έχει αποδεκτή μορφή.");
-			Translate<EqualValidator>("'Το πεδίο '{PropertyName}' πρέπει να έχει τιμή ίση με '{ComparisonValue}'.");
-			Translate<ExactLengthValidator>("'Το πεδίο '{PropertyName}' πρέπει να έχει μήκος ίσο με {MaxLength} χαρακτήρες. Έχετε καταχωρίσει {TotalLength} χαρακτήρες.");
-			Translate<InclusiveBetweenValidator>("'Το πεδίο '{PropertyName}' πρέπει να έχει τιμή μεταξύ {From} και {To}. Καταχωρίσατε την τιμή {Value}.");
-			Translate<ExclusiveBetweenValidator>("'Το πεδίο '{PropertyName}' πρέπει να έχει τιμή μεγαλύτερη από {From} και μικρότερη από {To}. Καταχωρίσατε την τιμή  {Value}.");
-			Translate<CreditCardValidator>("'Το πεδίο '{PropertyName}' δεν περιέχει αποδεκτό αριθμό πιστωτικής κάρτας.");
+			Translate<EqualValidator>("Το πεδίο '{PropertyName}' πρέπει να έχει τιμή ίση με '{ComparisonValue}'.");
+			Translate<ExactLengthValidator>("Το πεδίο '{PropertyName}' πρέπει να έχει μήκος ίσο με {MaxLength} χαρακτήρες. Έχετε καταχωρίσει {TotalLength} χαρακτήρες.");
+			Translate<InclusiveBetweenValidator>("Το πεδίο '{PropertyName}' πρέπει να έχει τιμή μεταξύ {From} και {To}. Καταχωρίσατε την τιμή {Value}.");
+			Translate<ExclusiveBetweenValidator>("Το πεδίο '{PropertyName}' πρέπει να έχει τιμή μεγαλύτερη από {From} και μικρότερη από {To}. Καταχωρίσατε την τιμή  {Value}.");
+			Translate<CreditCardValidator>("Το πεδίο '{PropertyName}' δεν περιέχει αποδεκτό αριθμό πιστωτικής κάρτας.");
 			Translate<ScalePrecisionValidator>("'Το πεδίο '{PropertyName}' δεν μπορεί να έχει περισσότερα από {expectedPrecision} ψηφία στο σύνολο, με μέγιστο επιτρεπόμενο αριθμό δεκαδικών τα {expectedScale} ψηφία. Έχετε καταχωρίσει {digits} ψηφία συνολικά με {actualScale} δεκαδικά.");
-			Translate<EmptyValidator>("'Το πεδίο '{PropertyName}' πρέπει να είναι κενό.");
-			Translate<NullValidator>("'Το πεδίο '{PropertyName}' πρέπει να είναι κενό.");
-			Translate<EnumValidator>("'Το πεδίο '{PropertyName}' επιτρέπει συγκεκριμένο εύρος τιμών που δεν περιλαμβάνουν την τιμή '{PropertyValue}' που καταχωρίσατε.");
+			Translate<EmptyValidator>("Το πεδίο '{PropertyName}' πρέπει να είναι κενό.");
+			Translate<NullValidator>("Το πεδίο '{PropertyName}' πρέπει να είναι κενό.");
+			Translate<EnumValidator>("Το πεδίο '{PropertyName}' επιτρέπει συγκεκριμένο εύρος τιμών που δεν περιλαμβάνουν την τιμή '{PropertyValue}' που καταχωρίσατε.");
 		}
 	}
 }
