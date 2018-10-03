563b2ab9ddeabe26745806526f849231910ff6ab
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2018-10-03 16:03:33 +0100

    Fix typos and set culture globally rather than per-test. Add missing license header.

--- src/FluentValidation.Tests/MessageFormatterTests.cs
+++ src/FluentValidation.Tests/MessageFormatterTests.cs
@@ -13,22 +13,20 @@
 // See the License for the specific language governing permissions and 
 // limitations under the License.
 // 
-// The latest version of this file can be found at https://github.com/jeremyskinner/FluentValidation
+// The latest version of this file can be found at https://github.com/JeremySkinner/FluentValidation
 #endregion
 
 namespace FluentValidation.Tests {
-	using Internal;
 	using System;
 	using System.Collections.Generic;
-	using System.Globalization;
-	using System.Threading;
+	using Internal;
 	using Xunit;
-
 
 	public class MessageFormatterTests {
 		MessageFormatter formatter;
 
 		public MessageFormatterTests() {
+			CultureScope.SetDefaultCulture();
 			formatter = new MessageFormatter();
 		}
 
@@ -61,65 +59,55 @@ namespace FluentValidation.Tests {
 		}
 
 		[Fact]
-		public void Format_property_value()	{
-			using (new CultureScope("en-US")) {
-				string result = formatter
+		public void Format_property_value() {
+			string result = formatter
 				.AppendPropertyValue(123.45)
 				.BuildMessage("{PropertyValue:#.#}");
 
-				result.ShouldEqual("123.5");
-			}
+			result.ShouldEqual("123.5");
 		}
 
 		[Fact]
 		public void Understands_numeric_formats() {
-			using (new CultureScope("en-US")) {
-				string result = formatter
-					.AppendArgument("d", 123)
-					.AppendArgument("e", 1052.0329112756)
-					.AppendArgument("c", -123.456789)
-					.AppendArgument("f", 1234.567)
-					.AppendArgument("p", 0.912)
-					.BuildMessage("{c:c3} {c:c4} {f:f} {p:p} {d:d} {e:e} {e:e2} {d:0000}");
+			string result = formatter
+				.AppendArgument("d", 123)
+				.AppendArgument("e", 1052.0329112756)
+				.AppendArgument("c", -123.456789)
+				.AppendArgument("f", 1234.567)
+				.AppendArgument("p", 0.912)
+				.BuildMessage("{c:c3} {c:c4} {f:f} {p:p} {d:d} {e:e} {e:e2} {d:0000}");
 
-				result.ShouldEqual($"{-123.456789:c3} {-123.456789:c4} {1234.567:f} {0.912:p} {123:d} {1052.0329112756:e} {1052.0329112756:e2} {123:0000}");
-			}
+			result.ShouldEqual($"{-123.456789:c3} {-123.456789:c4} {1234.567:f} {0.912:p} {123:d} {1052.0329112756:e} {1052.0329112756:e2} {123:0000}");
 		}
 
 		[Fact]
 		public void Adds_formatted_argument_and_custom_arguments() {
-			using (new CultureScope("en-US")) {
-				string result = formatter
+			string result = formatter
 				.AppendArgument("foo", 123.43)
 				.AppendAdditionalArguments("baz")
 				.BuildMessage("{foo:#.#} {0}");
 
-				result.ShouldEqual("123.4 baz");
-			}
+			result.ShouldEqual("123.4 baz");
 		}
 
 		[Fact]
 		public void Adds_formatted_argument_and_formatted_custom_arguments() {
-			using (new CultureScope("en-US")) {
-				string result = formatter
+			string result = formatter
 				.AppendArgument("foo", 123.43)
 				.AppendAdditionalArguments(.6789)
 				.BuildMessage("{foo:#.#} {0:p1}");
 
-				result.ShouldEqual($"{123.43:#.#} {.6789:p1}");
-			}
+			result.ShouldEqual($"{123.43:#.#} {.6789:p1}");
 		}
 
 		[Fact]
 		public void Understands_date_formats() {
 			var now = DateTime.Now;
-			using (new CultureScope("en-US")) {
-				string result = formatter
-					.AppendArgument("now", now)
-					.BuildMessage("{now:g} {now:MM-dd-yy} {now:f}");
+			string result = formatter
+				.AppendArgument("now", now)
+				.BuildMessage("{now:g} {now:MM-dd-yy} {now:f}");
 
-				result.ShouldEqual($"{now:g} {now:MM-dd-yy} {now:f}");
-			}
+			result.ShouldEqual($"{now:g} {now:MM-dd-yy} {now:f}");
 		}
 
 		[Fact]
@@ -133,7 +121,7 @@ namespace FluentValidation.Tests {
 		}
 
 		[Fact]
-		public void Should_not_call_formatted_arguments_when_nonexistant() {
+		public void Should_not_call_formatted_arguments_when_nonexistent() {
 			var mock = new FormatterMock();
 
 			string result = mock
@@ -146,7 +134,7 @@ namespace FluentValidation.Tests {
 		}
 
 		[Fact]
-		public void Should_call_formatted_arguments_when_existant()	{
+		public void Should_call_formatted_arguments_when_existent() {
 			var mock = new FormatterMock();
 
 			string result = mock
@@ -161,6 +149,7 @@ namespace FluentValidation.Tests {
 		private class FormatterMock : MessageFormatter {
 			public bool ReplacePlaceholdersWithValuesCalled { get; set; }
 			public bool ReplacePlaceholderWithValueCalled { get; set; }
+
 			protected override string ReplacePlaceholdersWithValues(string template, IDictionary<string, object> values) {
 				ReplacePlaceholdersWithValuesCalled = true;
 				return base.ReplacePlaceholdersWithValues(template, values);
@@ -170,7 +159,6 @@ namespace FluentValidation.Tests {
 				ReplacePlaceholderWithValueCalled = true;
 				return base.ReplacePlaceholderWithValue(template, key, value);
 			}
-
 		}
 	}
 }
