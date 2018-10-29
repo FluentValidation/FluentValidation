d11edfa99f81b595b297256878605ce67440c66b
Amund Skullerud <amund@flytit.no>
2018-10-29 09:54:37 +0100

    Add language Norwegian Bokmal (#933)

--- src/FluentValidation/Resources/LanguageManager.cs
+++ src/FluentValidation/Resources/LanguageManager.cs
@@ -63,6 +63,7 @@ namespace FluentValidation.Resources {
 				new ArabicLanguage(),
 				new AlbanianLanguage(),
 				new GreekLanguage(),
+				new NorwegianBokmalLanguage(),
 			};
 
 			foreach (var language in languages) {
---
+++ src/FluentValidation/Resources/Languages/NorwegianBokmalLanguage.cs
@@ -0,0 +1,51 @@
+#region License
+// Copyright (c) Jeremy Skinner (http://www.jeremyskinner.co.uk)
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at 
+// 
+// http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+// 
+// The latest version of this file can be found at https://github.com/JeremySkinner/FluentValidation
+#endregion
+
+namespace FluentValidation.Resources {
+	using Validators;
+
+	internal class NorwegianBokmalLanguage : Language {
+		public override string Name => "nb";
+
+		public NorwegianBokmalLanguage() {
+			Translate<EmailValidator>("'{PropertyName}' er ikke en gyldig e-postadresse.");
+			Translate<GreaterThanOrEqualValidator>("'{PropertyName}' skal være større enn eller lik '{ComparisonValue}'.");
+			Translate<GreaterThanValidator>("'{PropertyName}' skal være større enn '{ComparisonValue}'.");
+			Translate<LengthValidator>("'{PropertyName}' skal være mellom {MinLength} og {MaxLength} tegn. Du har tastet inn {TotalLength} tegn.");
+			Translate<MinimumLengthValidator>("'{PropertyName}' skal være større enn eller lik {MinLength} tegn. Du tastet inn {TotalLength} tegn.");
+			Translate<MaximumLengthValidator>("'{PropertyName}' skal være mindre enn eller lik {MaxLength} tegn. Du tastet inn {TotalLength} tegn.");
+			Translate<LessThanOrEqualValidator>("'{PropertyName}' skal være mindre enn eller lik '{ComparisonValue}'.");
+			Translate<LessThanValidator>("'{PropertyName}' skal være mindre enn '{ComparisonValue}'.");
+			Translate<NotEmptyValidator>("'{PropertyName}' kan ikke være tom.");
+			Translate<NotEqualValidator>("'{PropertyName}' kan ikke være lik med '{ComparisonValue}'.");
+			Translate<NotNullValidator>("'{PropertyName}' kan ikke være tom.");
+			Translate<PredicateValidator>("Den angitte betingelsen var ikke oppfylt for '{PropertyName}'.");
+			Translate<AsyncPredicateValidator>("Den angitte betingelsen var ikke oppfylt for '{PropertyName}'.");
+			Translate<RegularExpressionValidator>("'{PropertyName}' har ikke riktig format.");
+			Translate<EqualValidator>("'{PropertyName}' skal være lik med '{ComparisonValue}'.");
+			Translate<ExactLengthValidator>("'{PropertyName}' skal være {MaxLength} tegn langt. Du har tastet inn {TotalLength} tegn.");
+			Translate<InclusiveBetweenValidator>("'{PropertyName}' skal være mellom {From} og {To}. Du har tastet inn {Value}.");
+			Translate<ExclusiveBetweenValidator>("'{PropertyName}' skal være mellom {From} og {To} (eksklusiv). Du har tastet inn {Value}.");
+			Translate<CreditCardValidator>("'{PropertyName}' er ikke et gyldig kredittkortnummer.");
+			Translate<ScalePrecisionValidator>("'{PropertyName}' kan ikke være mer enn {expectedPrecision} siffer totalt, med hensyn til {expectedScale} desimaler. {digits} siffer og {actualScale} desimaler ble funnet.");
+			Translate<EmptyValidator>("'{PropertyName}' skal være tomt.");
+			Translate<NullValidator>("'{PropertyName}' skal være tomt.");
+			Translate<EnumValidator>("'{PropertyName}' har en rekke verdier men inneholder ikke '{PropertyValue}'.");
+		}
+	}
+}
