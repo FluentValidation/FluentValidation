5542b3efe58072954dcf8501d4d04893de583b8d
nsarris <nsarris@gmail.com>
2019-02-18 23:28:17 +0200

    ServiceCollecionExtension Tests

---
+++ src/FluentValidation.Tests.AspNetCore/ServiceCollectionExtensionTests.cs
@@ -0,0 +1,30 @@
+﻿#if NETCOREAPP || NETSTANDARD
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FluentValidation;
+
+namespace FluentValidation.Tests
+{
+
+	public class ServiceCollectionExtensionTests
+	{
+		class TestClass { }
+		class TestValidator : AbstractValidator<TestClass> { }
+
+		[Fact]
+		public void ServiceCollectionExtensionTests_should_pass_if_validator_is_resolved()
+		{
+			var serviceProvider = new ServiceCollection()
+				.AddValidatorsFromAssemblyContaining<TestValidator>()
+				.BuildServiceProvider();
+
+			serviceProvider.GetService<TestValidator>().ShouldNotBeNull();
+		}
+	}
+}
+#endif
