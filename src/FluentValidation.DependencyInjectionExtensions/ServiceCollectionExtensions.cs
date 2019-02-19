ef0e64495c64ae2e1015b8d3fc851e153ac35e42
nsarris <nsarris@gmail.com>
2019-02-19 21:13:24 +0200

    Used ServiceCollectionExtensions in MVC implementation

--- src/FluentValidation.DependencyInjectionExtensions/ServiceCollectionExtensions.cs
+++ src/FluentValidation.DependencyInjectionExtensions/ServiceCollectionExtensions.cs
@@ -9,11 +9,26 @@ namespace FluentValidation
 	public static class ServiceCollectionExtensions
 	{
 		/// <summary>
+		/// Adds all validators in specified assemblies
+		/// </summary>
+		/// <param name="services">The collection of services</param>
+		/// <param name="assembly">The assemblies to scan</param>
+		/// <param name="lifetime">The lifetime of the validators. The default is transient</param>
+		/// <returns></returns>
+		public static IServiceCollection AddValidatorsFromAssemblies(this IServiceCollection services, IEnumerable<Assembly> assemblies, ServiceLifetime lifetime = ServiceLifetime.Transient)
+		{
+			foreach (var assembly in assemblies)
+				services.AddValidatorsFromAssembly(assembly, lifetime);
+
+			return services;
+		}
+
+		/// <summary>
 		/// Adds all validators in specified assembly
 		/// </summary>
 		/// <param name="services">The collection of services</param>
 		/// <param name="assembly">The assembly to scan</param>
-		/// <param name="lifetime">The lifetime the validator is registered. The default is transient</param>
+		/// <param name="lifetime">The lifetime of the validators. The default is transient</param>
 		/// <returns></returns>
 		public static IServiceCollection AddValidatorsFromAssembly(this IServiceCollection services, Assembly assembly, ServiceLifetime lifetime = ServiceLifetime.Transient)
 		{
@@ -29,7 +44,7 @@ namespace FluentValidation
 		/// </summary>
 		/// <param name="services">The collection of services</param>
 		/// <param name="type">The type whose assembly to scan</param>
-		/// <param name="lifetime">The lifetime the validator is registered. The default is transient</param>
+		/// <param name="lifetime">The lifetime of the validators. The default is transient</param>
 		/// <returns></returns>
 		public static IServiceCollection AddValidatorsFromAssemblyContaining(this IServiceCollection services, Type type, ServiceLifetime lifetime = ServiceLifetime.Transient)
 			=> services.AddValidatorsFromAssembly(type.Assembly, lifetime);
@@ -38,7 +53,7 @@ namespace FluentValidation
 		/// Adds all validators in the assembly of the type specified by the generic parameter
 		/// </summary>
 		/// <param name="services">The collection of services</param>
-		/// <param name="lifetime">The lifetime the validator is registered. The default is transient</param>
+		/// <param name="lifetime">The lifetime of the validators. The default is transient</param>
 		/// <returns></returns>
 		public static IServiceCollection AddValidatorsFromAssemblyContaining<T>(this IServiceCollection services, ServiceLifetime lifetime = ServiceLifetime.Transient)
 			=> services.AddValidatorsFromAssembly(typeof(T).Assembly, lifetime);
@@ -48,7 +63,7 @@ namespace FluentValidation
 		/// </summary>
 		/// <param name="services">The collection of services</param>
 		/// <param name="scanResult">The scan result</param>
-		/// <param name="lifetime">The lifetime the validator is registered. The default is transient</param>
+		/// <param name="lifetime">The lifetime of the validators. The default is transient</param>
 		/// <returns></returns>
 		private static IServiceCollection AddScanResult(this IServiceCollection services, AssemblyScanner.AssemblyScanResult scanResult, ServiceLifetime lifetime)
 		{
