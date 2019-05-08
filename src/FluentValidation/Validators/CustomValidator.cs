8eb8800c308e81f6bec019f65ad58b1250431e85
SergerGood <serzher@inbox.ru>
2019-05-08 11:22:42 +0300

    avoiding the deadlock

--- src/FluentValidation/Validators/CustomValidator.cs
+++ src/FluentValidation/Validators/CustomValidator.cs
@@ -33,7 +33,7 @@
 			_isAsync = true;
 			_asyncAction = asyncAction;
 			//TODO: For FV 9, throw an exception by default if async validator is being executed synchronously.
-			_action = (x, ctx) => _asyncAction(x, ctx, new CancellationToken()).GetAwaiter().GetResult();
+            _action = (x, ctx) => Task.Run(() => _asyncAction(x, ctx, new CancellationToken())).GetAwaiter().GetResult();
 		}
 
 		public override IEnumerable<ValidationFailure> Validate(PropertyValidatorContext context) {
