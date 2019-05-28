df4278fc7993c800079034ce9007075bd2aca598
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2019-05-28 16:58:17 +0100

    Remove unneded localization tests and cleanup

--- src/FluentValidation.Tests/LocalisedNameTester.cs
+++ src/FluentValidation.Tests/LocalisedNameTester.cs
@@ -1,34 +1,32 @@
 #region License
+
 // Copyright (c) Jeremy Skinner (http://www.jeremyskinner.co.uk)
-// 
-// Licensed under the Apache License, Version 2.0 (the "License"); 
-// you may not use this file except in compliance with the License. 
-// You may obtain a copy of the License at 
-// 
-// http://www.apache.org/licenses/LICENSE-2.0 
-// 
-// Unless required by applicable law or agreed to in writing, software 
-// distributed under the License is distributed on an "AS IS" BASIS, 
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-// See the License for the specific language governing permissions and 
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 // The latest version of this file can be found at https://github.com/JeremySkinner/FluentValidation
+
 #endregion
 
 namespace FluentValidation.Tests {
-    using System.ComponentModel.DataAnnotations;
-    using System.Globalization;
-    using Xunit;
-    using System.Linq;
-    using Resources;
-    using System;
+	using System;
+	using System.Linq;
+	using Xunit;
 
-    public class LocalisedNameTester : IDisposable {
-
-	    public LocalisedNameTester() {
-		    CultureScope.SetDefaultCulture();
-	    }
+	public class LocalisedNameTester : IDisposable {
+		public LocalisedNameTester() {
+			CultureScope.SetDefaultCulture();
+		}
 
 		public void Dispose() {
 			CultureScope.SetDefaultCulture();
@@ -54,38 +52,9 @@ namespace FluentValidation.Tests {
 			result.Errors.Single().ErrorMessage.ShouldEqual("'foo' must not be empty.");
 		}
 
-		[Fact]
-		public void Uses_localized_name_from_display_attribute() {
-			using (new CultureScope("en-us")) {
-				var validator = new InlineValidator<Person2> {
-					v => v.RuleFor(x => x.Name).NotNull().WithMessage("{PropertyName}")
-				};
-
-				var result = validator.Validate(new Person2());
-				result.Errors[0].ErrorMessage.ShouldEqual("foo");
-
-				using (new CultureScope("fr-FR")) {
-					result = validator.Validate(new Person2());
-					result.Errors[0].ErrorMessage.ShouldEqual("bar");
-				}
-			}
-		}
-
-		public class Person2 {
-			[Display(ResourceType = typeof(TestMessages), Name = "PropertyName")]
-			public string Name { get; set; }
-			 
-		}
-
 		public static class MyResources {
 			public static string CustomProperty {
 				get { return "foo"; }
-			}
-		}
-
-		public static class OverrideResources {
-			public static string CustomProperty {
-				get { return "bar"; }
 			}
 		}
 	}
