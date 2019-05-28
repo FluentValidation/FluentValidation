a10adbdf8571e58a7a0a2fd96adf068cb12e3f02
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2019-05-28 16:29:43 +0100

    Remove the old metadata interfaces

--- src/FluentValidation/Validators/RegularExpressionValidator.cs
+++ src/FluentValidation/Validators/RegularExpressionValidator.cs
@@ -1,18 +1,18 @@
 #region License
 // Copyright (c) Jeremy Skinner (http://www.jeremyskinner.co.uk)
-// 
-// Licensed under the Apache License, Version 2.0 (the "License"); 
-// you may not use this file except in compliance with the License. 
-// You may obtain a copy of the License at 
-// 
-// http://www.apache.org/licenses/LICENSE-2.0 
-// 
-// Unless required by applicable law or agreed to in writing, software 
-// distributed under the License is distributed on an "AS IS" BASIS, 
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-// See the License for the specific language governing permissions and 
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 // The latest version of this file can be found at https://github.com/jeremyskinner/FluentValidation
 #endregion
 
@@ -21,7 +21,7 @@ namespace FluentValidation.Validators {
 	using System.Text.RegularExpressions;
 	using Resources;
 
-	public class RegularExpressionValidator : PropertyValidator, IRegularExpressionValidator {
+	public class RegularExpressionValidator : PropertyValidator {
 		readonly Func<object, Regex> _regexFunc;
 
 		public RegularExpressionValidator(string expression) :base(new LanguageStringSource(nameof(RegularExpressionValidator))) {
@@ -56,7 +56,7 @@ namespace FluentValidation.Validators {
 
 		protected override bool IsValid(PropertyValidatorContext context) {
 			var regex = _regexFunc(context.InstanceToValidate);
-			
+
 			if (regex != null && context.PropertyValue != null && !regex.IsMatch((string) context.PropertyValue)) {
 				context.MessageFormatter.AppendArgument("RegularExpression", regex.ToString());
 				return false;
@@ -69,9 +69,5 @@ namespace FluentValidation.Validators {
 		}
 
 		public string Expression { get; }
-	}
-
-	public interface IRegularExpressionValidator : IPropertyValidator {
-		string Expression { get; }
 	}
 }
