679d0e08600fb5fb29a3f105188732ed06c64dba
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2019-06-12 11:12:09 +0100

    Simplify TestHelper internals and advanced syntax (#1148)

--- src/FluentValidation.Tests/ValidatorTesterTester.cs
+++ src/FluentValidation.Tests/ValidatorTesterTester.cs
@@ -1,19 +1,19 @@
 #region License
 
 // Copyright (c) Jeremy Skinner (http://www.jeremyskinner.co.uk)
-// 
-// Licensed under the Apache License, Version 2.0 (the "License"); 
-// you may not use this file except in compliance with the License. 
-// You may obtain a copy of the License at 
-// 
-// http://www.apache.org/licenses/LICENSE-2.0 
-// 
-// Unless required by applicable law or agreed to in writing, software 
-// distributed under the License is distributed on an "AS IS" BASIS, 
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-// See the License for the specific language governing permissions and 
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 // The latest version of this file can be found at https://github.com/jeremyskinner/FluentValidation
 
 #endregion
@@ -210,6 +210,54 @@ namespace FluentValidation.Tests {
 				.WithErrorCode("NotNullValidator");
 			assertionRoot.Property(x => x.Surname).ShouldHaveValidationError().WithErrorCode("NotNullValidator");
 			assertionRoot.Property(x => x.Id).ShouldNotHaveValidationError();
+		}
+
+		[Fact]
+		public void Tests_nested_property_using_obsolete_method() {
+			var validator = new TestValidator();
+			validator.RuleFor(x => x.Address.Line1).NotEqual("foo");
+
+			var result = validator.TestValidate(new Person() {
+				Address = new Address {Line1 = "bar"}
+			});
+
+			var ex = Assert.Throws<ValidationTestException>(() => {
+				result.Which.Property(x => x.Address).Property(x => x.Line1).ShouldHaveValidationError();
+			});
+
+			ex.Message.ShouldEqual("Expected a validation error for property Address.Line1");
+		}
+
+		[Fact]
+		public void Tests_nested_property() {
+			var validator = new TestValidator();
+			validator.RuleFor(x => x.Address.Line1).NotEqual("foo");
+
+			var result = validator.TestValidate(new Person() {
+				Address = new Address {Line1 = "bar"}
+			});
+
+			var ex = Assert.Throws<ValidationTestException>(() => {
+				result.ShouldHaveValidationErrorFor(x => x.Address.Line1);
+			});
+
+			ex.Message.ShouldEqual("Expected a validation error for property Address.Line1");
+		}
+
+		[Fact]
+		public void Tests_nested_property_reverse() {
+			var validator = new TestValidator();
+			validator.RuleFor(x => x.Address.Line1).NotEqual("foo");
+
+			var result = validator.TestValidate(new Person() {
+				Address = new Address {Line1 = "foo"}
+			});
+
+			var ex = Assert.Throws<ValidationTestException>(() => {
+				result.ShouldNotHaveValidationErrorFor(x => x.Address.Line1);
+			});
+
+			ex.Message.Contains($"Expected no validation errors for property Address.Line1").ShouldBeTrue();
 		}
 
 		[Fact]
@@ -475,13 +523,13 @@ namespace FluentValidation.Tests {
 		[Fact]
 		public void ShouldHaveChildValidator_should_work_with_DependentRules() {
 			var validator = new InlineValidator<Person>();
-			
+
 			validator.RuleFor(x => x.Children)
 				.NotNull().When(p => true)
 				.DependentRules(() => {
 					validator.RuleForEach(p => p.Children).SetValidator(p => new InlineValidator<Person>());
 				});
-			
+
 			validator.ShouldHaveChildValidator(x => x.Children, typeof(InlineValidator<Person>));
 		}
 
@@ -494,9 +542,61 @@ namespace FluentValidation.Tests {
 
 			var person = new Person() { Surname = "c" };
 			var result = validator.TestValidate(person);
-    
+
 			result.ShouldHaveError().WithErrorCode("nota");
 			result.ShouldHaveError().WithErrorCode("notb");
+		}
+
+		[Fact]
+		public void Matches_any_failure() {
+			var validator = new InlineValidator<Person> {
+				v => v.RuleFor(x => x.Surname).NotEqual("foo"),
+			};
+
+			var resultWithFailure = validator.TestValidate(new Person { Surname = "foo"});
+			var resultWithoutFailure = validator.TestValidate(new Person { Surname = ""});
+
+			Assert.Throws<ValidationTestException>(() => resultWithoutFailure.ShouldHaveAnyValidationError());
+			Assert.Throws<ValidationTestException>(() => resultWithFailure.ShouldNotHaveAnyValidationErrors());
+
+			// Neither should throw.
+			resultWithFailure.ShouldHaveAnyValidationError();
+			resultWithoutFailure.ShouldNotHaveAnyValidationErrors();
+		}
+
+		[Fact]
+		public void Model_level_check_fails_if_no_model_level_failures() {
+			var validator = new InlineValidator<Person>();
+			validator.RuleFor(x => x.Surname).Must(x => false);
+			var result = validator.TestValidate(new Person());
+
+			Assert.Throws<ValidationTestException>(() => {
+				result.ShouldHaveValidationErrorFor(x => x);
+			});
+
+			Assert.Throws<ValidationTestException>(() => {
+				result.ShouldHaveValidationErrorFor("");
+			});
+		}
+
+		[Fact]
+		public void Matches_model_level_rule() {
+			var validator = new InlineValidator<Person>();
+			validator.RuleFor(x => x).Must(x => false);
+			validator.RuleFor(x => x.Surname).Must(x => false);
+
+			var result = validator.TestValidate(new Person());
+
+			bool thrown = false;
+			try {
+				result.ShouldHaveValidationErrorFor(x => x);
+				result.ShouldHaveValidationErrorFor("");
+			}
+			catch (ValidationTestException) {
+				thrown = true;
+			}
+
+			thrown.ShouldBeFalse();
 		}
 
 		private class AddressValidator : AbstractValidator<Address> {
--- src/FluentValidation/TestHelper/TestPropertyChain.cs
+++ src/FluentValidation/TestHelper/TestPropertyChain.cs
@@ -1,19 +1,19 @@
 #region License
 
 // Copyright (c) Jeremy Skinner (http://www.jeremyskinner.co.uk)
-//  
-// Licensed under the Apache License, Version 2.0 (the "License"); 
-// You may not use this file except in compliance with the License. 
-// You may obtain a copy of the License at 
-// 
-// http://www.apache.org/licenses/LICENSE-2.0 
-// 
-// Unless required by applicable law or agreed to in writing, software 
-// distributed under the License is distributed on an "AS IS" BASIS, 
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-// See the License for the specific language governing permissions and 
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-//  
+//
 // The latest version of this file can be found at http://github.com/JeremySkinner/FluentValidation
 
 #endregion
@@ -27,6 +27,7 @@ namespace FluentValidation.TestHelper {
 	using Internal;
 	using Results;
 
+	[Obsolete]
 	internal class TestPropertyChain<TValue, TValue1> : ITestPropertyChain<TValue> {
 		private readonly IValidationResultTester _validationResultTester;
 		private readonly IEnumerable<MemberInfo> _properties;
@@ -37,7 +38,7 @@ namespace FluentValidation.TestHelper {
 		}
 
 		public ITestPropertyChain<TValue2> Property<TValue2>(Expression<Func<TValue, TValue2>> memberAccessor) {
-			return new TestPropertyChain<TValue2, TValue1>(_validationResultTester, _properties.Concat(new[] {((MemberAccessor<TValue, TValue2>) memberAccessor).Member}));
+			return new TestPropertyChain<TValue2, TValue1>(_validationResultTester, _properties.Concat(new[] { memberAccessor.GetMember() }));
 		}
 
 		public IEnumerable<ValidationFailure> ShouldHaveValidationError() {
