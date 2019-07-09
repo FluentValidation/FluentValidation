bd286cbb1a7640819db58c517112d762955a5e22
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2019-07-09 11:38:05 +0100

    Add missing culture constant

--- src/FluentValidation/Resources/Languages/WelshLanguage.cs
+++ src/FluentValidation/Resources/Languages/WelshLanguage.cs
@@ -1,18 +1,18 @@
 #region License
 // Copyright (c) Jeremy Skinner (http://www.jeremyskinner.co.uk)
-// 
-// Licensed under the Apache License, Version 2.0 (the "License"); 
-// you may not use this file except in compliance with the License. 
-// You may obtain a copy of the License at 
-// 
-// http://www.apache.org/licenses/LICENSE-2.0 
-// 
-// Unless required by applicable law or agreed to in writing, software 
-// distributed under the License is distributed on an "AS IS" BASIS, 
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-// See the License for the specific language governing permissions and 
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 // The latest version of this file can be found at https://github.com/JeremySkinner/FluentValidation
 #endregion
 
@@ -20,7 +20,8 @@ namespace FluentValidation.Resources {
   using Validators;
 
   internal class WelshLanguage : Language {
-    public override string Name => "cy";
+	  public const string Culture = "cy";
+    public override string Name => Culture;
 
     public WelshLanguage() {
       Translate<EmailValidator>("Nid yw '{PropertyName}' yn gyfeiriad e-bost dilys.");
