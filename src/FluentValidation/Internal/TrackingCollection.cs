0556596ab4dfb59e4fe69739608753ecd9b5fc40
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2019-08-13 10:17:22 +0100

    Indicate that TrackingCollection will be made internal

--- src/FluentValidation/Internal/TrackingCollection.cs
+++ src/FluentValidation/Internal/TrackingCollection.cs
@@ -1,18 +1,18 @@
 #region License
 // Copyright (c) Jeremy Skinner (http://www.jeremyskinner.co.uk)
-// 
-// Licensed under the Apache License, Version 2.0 (the "License"); 
-// you may not use this file except in compliance with the License. 
-// You may obtain a copy of the License at 
-// 
-// http://www.apache.org/licenses/LICENSE-2.0 
-// 
-// Unless required by applicable law or agreed to in writing, software 
-// distributed under the License is distributed on an "AS IS" BASIS, 
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-// See the License for the specific language governing permissions and 
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 // The latest version of this file can be found at https://github.com/JeremySkinner/FluentValidation
 #endregion
 
@@ -22,6 +22,7 @@ namespace FluentValidation.Internal {
 	using System.Collections;
 	using System.Collections.Generic;
 
+	[Obsolete("This class will be internal in FluentValidation 9.")]
 	public class TrackingCollection<T> : IEnumerable<T> {
 		readonly List<T> _innerCollection = new List<T>();
 		public event Action<T> ItemAdded;
@@ -56,7 +57,7 @@ namespace FluentValidation.Internal {
 		public void AddRange(IEnumerable<T> collection) {
 			_innerCollection.AddRange(collection);
 		}
-		
+
 		public IEnumerator<T> GetEnumerator() {
 			return _innerCollection.GetEnumerator();
 		}
