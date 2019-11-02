d21ea15a128322db78272ee872ad8594ee64c457
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2019-11-02 11:32:33 +0000

    Minor tidy up NoopPropertyValidator.ValidateAsync

--- src/FluentValidation/Validators/NoopPropertyValidator.cs
+++ src/FluentValidation/Validators/NoopPropertyValidator.cs
@@ -1,18 +1,18 @@
 #region License
 // Copyright (c) Jeremy Skinner (http://www.jeremyskinner.co.uk)
-// 
-// Licensed under the Apache License, Version 2.0 (the "License"); 
-// you may not use this file except in compliance with the License. 
-// You may obtain a copy of the License at 
-// 
-// http://www.apache.org/licenses/LICENSE-2.0 
-// 
-// Unless required by applicable law or agreed to in writing, software 
-// distributed under the License is distributed on an "AS IS" BASIS, 
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-// See the License for the specific language governing permissions and 
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 // The latest version of this file can be found at https://github.com/jeremyskinner/FluentValidation
 #endregion
 
@@ -27,16 +27,14 @@ namespace FluentValidation.Validators {
 	public abstract class NoopPropertyValidator : IPropertyValidator {
 		public abstract IEnumerable<ValidationFailure> Validate(PropertyValidatorContext context);
 
-#pragma warning disable 1998
-		public virtual async Task<IEnumerable<ValidationFailure>> ValidateAsync(PropertyValidatorContext context, CancellationToken cancellation) {
-			return Validate(context);
+		public virtual Task<IEnumerable<ValidationFailure>> ValidateAsync(PropertyValidatorContext context, CancellationToken cancellation) {
+			return Task.FromResult(Validate(context));
 		}
-#pragma warning restore 1998
 
 		public virtual bool ShouldValidateAsync(ValidationContext context) {
 			return false;
 		}
-		
+
 		public PropertyValidatorOptions Options { get; } = new PropertyValidatorOptions();
 	}
 }
