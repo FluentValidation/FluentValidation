5513b22394b73d1e3f38d948f464e46bac77884a
Nick Howell <nick.howell@bnsflogistics.com>
2020-03-30 21:28:22 -0400

    Change ScalePrecisionValidator to to check digits to left of decimal point
    Change the behavior of ScalePrecisionValidator to behave like SQL Server / MySQL / Oracle / etc. where the precision is the total number of digits and the scale is the number of digits to the right of the decimal. The difference between the precision and the scale should be the number of digits allowed on the left side of the decimal point.
    
    #1008

--- src/FluentValidation/Validators/ScalePrecisionValidator.cs
+++ src/FluentValidation/Validators/ScalePrecisionValidator.cs
@@ -22,21 +22,21 @@ namespace FluentValidation.Validators {
 	using System;
 	using Resources;
 
-	// Attribution: This class was contributed to FluentValidation using code posted on StackOverflow by Jon Skeet
-	// The original code can be found at https://stackoverflow.com/a/764102
+  // Attribution: This class was contributed to FluentValidation using code posted on StackOverflow by Jon Skeet
+  // The original code can be found at https://stackoverflow.com/a/764102
 
-	/// <summary>
-	/// Allows a decimal to be validated for scale and precision.
-	/// Scale would be the number of digits to the right of the decimal point.
-	/// Precision would be the number of digits.
-	///
-	/// It can be configured to use the effective scale and precision
-	/// (i.e. ignore trailing zeros) if required.
-	///
-	/// 123.4500 has an scale of 4 and a precision of 7, but an effective scale
-	/// and precision of 2 and 5 respectively.
-	/// </summary>
-	public class ScalePrecisionValidator : PropertyValidator {
+  /// <summary>
+  /// Allows a decimal to be validated for scale and precision.
+  /// Scale would be the number of digits to the right of the decimal point.
+  /// Precision would be the number of digits. This number includes both the left and the right sides of the decimal point.
+  ///
+  /// It can be configured to use the effective scale and precision
+  /// (i.e. ignore trailing zeros) if required.
+  ///
+  /// 123.4500 has an scale of 4 and a precision of 7, but an effective scale
+  /// and precision of 2 and 5 respectively.
+  /// </summary>
+  public class ScalePrecisionValidator : PropertyValidator {
 		public ScalePrecisionValidator(int scale, int precision) : base(new LanguageStringSource(nameof(ScalePrecisionValidator))) {
 			Init(scale, precision);
 		}
@@ -53,11 +53,13 @@ namespace FluentValidation.Validators {
 			if (decimalValue.HasValue) {
 				var scale = GetScale(decimalValue.Value);
 				var precision = GetPrecision(decimalValue.Value);
-				if (scale > Scale || precision > Precision) {
+        var actualIntegerDigits = precision - scale;
+        var expectedIntegerDigits = Precision - Scale;
+        if (scale > Scale || actualIntegerDigits > expectedIntegerDigits) {
 					context.MessageFormatter
 						.AppendArgument("ExpectedPrecision", Precision)
 						.AppendArgument("ExpectedScale", Scale)
-						.AppendArgument("Digits", precision - scale)
+						.AppendArgument("Digits", actualIntegerDigits)
 						.AppendArgument("ActualScale", scale);
 
 					return false;
@@ -122,18 +124,12 @@ namespace FluentValidation.Validators {
 		private int GetPrecision(decimal Decimal) {
 			// Precision: number of times we can divide by 10 before we get to 0
 			uint precision = 0;
-			if (Decimal != 0m) {
-				for (decimal tmp = GetMantissa(Decimal); tmp >= 1; tmp /= 10) {
-					precision++;
-				}
-
-				if (IgnoreTrailingZeros) {
-					return (int) (precision - NumTrailingZeros(Decimal));
-				}
+			for (decimal tmp = GetMantissa(Decimal); tmp >= 1; tmp /= 10) {
+				precision++;
 			}
-			else {
-				// Handle zero differently. It's odd.
-				precision = (uint) GetScale(Decimal) + 1;
+
+			if (IgnoreTrailingZeros) {
+				return (int) (precision - NumTrailingZeros(Decimal));
 			}
 
 			return (int) precision;
