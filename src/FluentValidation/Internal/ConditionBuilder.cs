a3310a60a9a57c497aac8eca65f706fd992fcd8b
David Cabiness <dcabiness@werktek.com>
2020-04-14 12:16:19 -0400

    casting instead of using ToGeneric

--- src/FluentValidation/Internal/ConditionBuilder.cs
+++ src/FluentValidation/Internal/ConditionBuilder.cs
@@ -30,59 +30,59 @@ namespace FluentValidation.Internal {
 			_rules = rules;
 		}
 
-    /// <summary>
-    /// Defines a condition that applies to several rules
-    /// </summary>
-    /// <param name="predicate">The condition that should apply to multiple rules</param>
-    /// <param name="action">Action that encapsulates the rules.</param>
-    /// <returns></returns>
-    public IConditionBuilder When(Func<T, ValidationContext<T>, bool> predicate, Action action) {
-      var propertyRules = new List<IValidationRule>();
+		/// <summary>
+		/// Defines a condition that applies to several rules
+		/// </summary>
+		/// <param name="predicate">The condition that should apply to multiple rules</param>
+		/// <param name="action">Action that encapsulates the rules.</param>
+		/// <returns></returns>
+		public IConditionBuilder When(Func<T, ValidationContext<T>, bool> predicate, Action action) {
+			var propertyRules = new List<IValidationRule>();
 
-      using (_rules.OnItemAdded(propertyRules.Add)) {
-        action();
-      }
+			using (_rules.OnItemAdded(propertyRules.Add)) {
+				action();
+			}
 
-      // Generate unique ID for this shared condition.
-      var id = "_FV_Condition_" + Guid.NewGuid();
+			// Generate unique ID for this shared condition.
+			var id = "_FV_Condition_" + Guid.NewGuid();
 
-      bool Condition(ValidationContext context) {
-        string cacheId = null;
+			bool Condition(ValidationContext context) {
+				string cacheId = null;
 
-        if (context.InstanceToValidate != null) {
-          cacheId = id + context.InstanceToValidate.GetHashCode();
+				if (context.InstanceToValidate != null) {
+					cacheId = id + context.InstanceToValidate.GetHashCode();
 
-          if (context.RootContextData.TryGetValue(cacheId, out var value)) {
-            if (value is bool result) {
-              return result;
-            }
-          }
-        }
+					if (context.RootContextData.TryGetValue(cacheId, out var value)) {
+						if (value is bool result) {
+							return result;
+						}
+					}
+				}
 
-        var executionResult = predicate((T)context.InstanceToValidate, context.ToGeneric<T>());
-        if (context.InstanceToValidate != null) {
-          context.RootContextData[cacheId] = executionResult;
-        }
-        return executionResult;
-      }
+				var executionResult = predicate((T)context.InstanceToValidate, (ValidationContext<T>)context);
+				if (context.InstanceToValidate != null) {
+					context.RootContextData[cacheId] = executionResult;
+				}
+				return executionResult;
+			}
 
-      // Must apply the predicate after the rule has been fully created to ensure any rules-specific conditions have already been applied.
-      foreach (var rule in propertyRules) {
-        rule.ApplySharedCondition(Condition);
-      }
+			// Must apply the predicate after the rule has been fully created to ensure any rules-specific conditions have already been applied.
+			foreach (var rule in propertyRules) {
+				rule.ApplySharedCondition(Condition);
+			}
 
-      return new ConditionOtherwiseBuilder(_rules, Condition);
-    }
+			return new ConditionOtherwiseBuilder(_rules, Condition);
+		}
 
-    /// <summary>
-    /// Defines an inverse condition that applies to several rules
-    /// </summary>
-    /// <param name="predicate">The condition that should be applied to multiple rules</param>
-    /// <param name="action">Action that encapsulates the rules</param>
-    public IConditionBuilder Unless(Func<T, ValidationContext<T>, bool> predicate, Action action) {
-      return When((x, context) => !predicate(x, context), action);
-    }
-  }
+		/// <summary>
+		/// Defines an inverse condition that applies to several rules
+		/// </summary>
+		/// <param name="predicate">The condition that should be applied to multiple rules</param>
+		/// <param name="action">Action that encapsulates the rules</param>
+		public IConditionBuilder Unless(Func<T, ValidationContext<T>, bool> predicate, Action action) {
+			return When((x, context) => !predicate(x, context), action);
+		}
+	}
 
 	internal class AsyncConditionBuilder<T> {
 		private TrackingCollection<IValidationRule> _rules;
@@ -97,51 +97,51 @@ namespace FluentValidation.Internal {
 		/// <param name="predicate">The asynchronous condition that should apply to multiple rules</param>
 		/// <param name="action">Action that encapsulates the rules.</param>
 		/// <returns></returns>
-    public IConditionBuilder WhenAsync(Func<T, ValidationContext<T>, CancellationToken, Task<bool>> predicate, Action action) {
-      var propertyRules = new List<IValidationRule>();
+		public IConditionBuilder WhenAsync(Func<T, ValidationContext<T>, CancellationToken, Task<bool>> predicate, Action action) {
+			var propertyRules = new List<IValidationRule>();
 
-      using (_rules.OnItemAdded(propertyRules.Add)) {
-        action();
-      }
+			using (_rules.OnItemAdded(propertyRules.Add)) {
+				action();
+			}
 
-      // Generate unique ID for this shared condition.
-      var id = "_FV_AsyncCondition_" + Guid.NewGuid();
+			// Generate unique ID for this shared condition.
+			var id = "_FV_AsyncCondition_" + Guid.NewGuid();
 
-      async Task<bool> Condition(ValidationContext context, CancellationToken ct) {
-        string cacheId = null;
-        if (context.InstanceToValidate != null) {
-          cacheId = id + context.InstanceToValidate.GetHashCode();
+			async Task<bool> Condition(ValidationContext context, CancellationToken ct) {
+				string cacheId = null;
+				if (context.InstanceToValidate != null) {
+					cacheId = id + context.InstanceToValidate.GetHashCode();
 
-          if (context.RootContextData.TryGetValue(cacheId, out var value)) {
-            if (value is bool result) {
-              return result;
-            }
-          }
-        }
+					if (context.RootContextData.TryGetValue(cacheId, out var value)) {
+						if (value is bool result) {
+							return result;
+						}
+					}
+				}
 
-        var executionResult = await predicate((T)context.InstanceToValidate, context.ToGeneric<T>(), ct);
-        if (context.InstanceToValidate != null) {
-          context.RootContextData[cacheId] = executionResult;
-        }
-        return executionResult;
-      }
+				var executionResult = await predicate((T)context.InstanceToValidate, (ValidationContext<T>)context, ct);
+				if (context.InstanceToValidate != null) {
+					context.RootContextData[cacheId] = executionResult;
+				}
+				return executionResult;
+			}
 
-      foreach (var rule in propertyRules) {
-        rule.ApplySharedAsyncCondition(Condition);
-      }
+			foreach (var rule in propertyRules) {
+				rule.ApplySharedAsyncCondition(Condition);
+			}
 
-      return new AsyncConditionOtherwiseBuilder(_rules, Condition);
-    }
+			return new AsyncConditionOtherwiseBuilder(_rules, Condition);
+		}
 
-    /// <summary>
-    /// Defines an inverse asynchronous condition that applies to several rules
-    /// </summary>
-    /// <param name="predicate">The asynchronous condition that should be applied to multiple rules</param>
-    /// <param name="action">Action that encapsulates the rules</param>
-    public IConditionBuilder UnlessAsync(Func<T, ValidationContext, CancellationToken, Task<bool>> predicate, Action action) {
-      return WhenAsync(async (x, context, ct) => !await predicate(x, context, ct), action);
-    }
-  }
+		/// <summary>
+		/// Defines an inverse asynchronous condition that applies to several rules
+		/// </summary>
+		/// <param name="predicate">The asynchronous condition that should be applied to multiple rules</param>
+		/// <param name="action">Action that encapsulates the rules</param>
+		public IConditionBuilder UnlessAsync(Func<T, ValidationContext, CancellationToken, Task<bool>> predicate, Action action) {
+			return WhenAsync(async (x, context, ct) => !await predicate(x, context, ct), action);
+		}
+	}
 
 	internal class ConditionOtherwiseBuilder : IConditionBuilder {
 		private TrackingCollection<IValidationRule> _rules;
