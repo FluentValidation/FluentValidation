65e79837d314031f57d4edd6e21c73fee3931b58
Hamed ZVand <sunrise.freedom@gmail.com>
2020-08-31 16:49:53 +0430

    Update PersianLanguage.cs (#1513)
    typo in `MinimumLengthValidator`

--- src/FluentValidation/Resources/Languages/PersianLanguage.cs
+++ src/FluentValidation/Resources/Languages/PersianLanguage.cs
@@ -28,7 +28,7 @@ namespace FluentValidation.Resources {
 			Translate<GreaterThanOrEqualValidator>("'{PropertyName}' باید بیشتر یا مساوی '{ComparisonValue}' باشد.");
 			Translate<GreaterThanValidator>("'{PropertyName}' باید بیشتر از '{ComparisonValue}' باشد.");
 			Translate<LengthValidator>("'{PropertyName}' باید حداقل {MinLength} و حداکثر {MaxLength} کاراکتر داشته باشد. اما مقدار وارد شده {TotalLength} کاراکتر دارد.");
-			Translate<MinimumLengthValidator>("'{PropertyName}' باید بزرگتر یا برابر با {MinLength} کاراکتر باشد. شما شخصیت {TotalLength} را وارد کردید");
+			Translate<MinimumLengthValidator>("'{PropertyName}' باید بزرگتر یا برابر با {MinLength} کاراکتر باشد. شما تعداد {TotalLength} کاراکتر را وارد کردید");
 			Translate<MaximumLengthValidator>("'{PropertyName}' باید کمتر یا مساوی {MaxLength} باشد. {TotalLength} را وارد کردید");
 			Translate<LessThanOrEqualValidator>("'{PropertyName}' باید کمتر یا مساوی '{ComparisonValue}' باشد.");
 			Translate<LessThanValidator>("'{PropertyName}' باید کمتر از '{ComparisonValue}' باشد.");
