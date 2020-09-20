72572d1c44fc8087701aa0d3f2001c5f86fc89e6
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2020-09-20 19:35:00 +0100

    Remove more deprecated code

--- src/FluentValidation.Tests/PredicateValidatorTester.cs
+++ src/FluentValidation.Tests/PredicateValidatorTester.cs
@@ -76,10 +76,6 @@ namespace FluentValidation.Tests {
 			error.AttemptedValue.ShouldEqual("test");
 			error.ErrorCode.ShouldEqual("PredicateValidator");
 
-#pragma warning disable 618
-			error.FormattedMessageArguments.Length.ShouldEqual(0);
-#pragma warning restore 618
-
 			error.FormattedMessagePlaceholderValues.Count.ShouldEqual(2);
 			error.FormattedMessagePlaceholderValues.ContainsKey("PropertyName").ShouldBeTrue();
 			error.FormattedMessagePlaceholderValues.ContainsKey("PropertyValue").ShouldBeTrue();
