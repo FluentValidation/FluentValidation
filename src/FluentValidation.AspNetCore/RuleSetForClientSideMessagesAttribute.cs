ad35d04d4933a6b86dee9234df0f02b680591fc7
David John Williams <dragonranger@hotmail.com>
2020-10-14 13:20:21 +0100

    Updated RuleSetForClientSideMessagesAttribute to work with Razor Pages PageModel (#1548)
    Co-authored-by: David Williams <David.Williams@iow.gov.uk>

--- src/FluentValidation.AspNetCore/RuleSetForClientSideMessagesAttribute.cs
+++ src/FluentValidation.AspNetCore/RuleSetForClientSideMessagesAttribute.cs
@@ -1,59 +1,42 @@
 namespace FluentValidation.AspNetCore {
 	using System;
-  using FluentValidation.Internal;
-  using Microsoft.AspNetCore.Http;
+	using Microsoft.AspNetCore.Http;
 	using Microsoft.AspNetCore.Mvc.Filters;
 
 	/// <summary>
 	/// Specifies which ruleset should be used when deciding which validators should be used to generate client-side messages.
 	/// </summary>
 	public class RuleSetForClientSideMessagesAttribute : ActionFilterAttribute {
-		private const string _key = "_FV_ClientSideRuleSet";
+
 		private readonly string[] _ruleSets;
 
-		public RuleSetForClientSideMessagesAttribute(string ruleSet) {
-			_ruleSets = new[] { ruleSet };
-		}
+		public RuleSetForClientSideMessagesAttribute(string ruleSet) => _ruleSets = new[] { ruleSet };
 
-		public RuleSetForClientSideMessagesAttribute(params string[] ruleSets) {
-			this._ruleSets = ruleSets;
-		}
+		public RuleSetForClientSideMessagesAttribute(params string[] ruleSets) => _ruleSets = ruleSets;
 
-		public override void OnActionExecuting(ActionExecutingContext filterContext) {
-			var contextAccessor = filterContext.HttpContext.RequestServices.GetService(typeof(IHttpContextAccessor));
+		public override void OnResultExecuting(ResultExecutingContext context) {
+			var contextAccessor = context.HttpContext.RequestServices.GetService(typeof(IHttpContextAccessor));
 
-			if(contextAccessor == null) {
+			if (contextAccessor == null) {
 				throw new InvalidOperationException("Cannot use the RuleSetForClientSideMessagesAttribute unless the IHttpContextAccessor is registered with the service provider. Make sure the provider is registered by calling services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>(); in your Startup class's ConfigureServices method");
 			}
 
-			SetRulesetForClientValidation(filterContext.HttpContext, _ruleSets);
+			context.HttpContext.SetRulesetForClientsideMessages(_ruleSets);
 		}
 
-		
 		/// <summary>
 		/// Allows the ruleset used for generating clientside metadata to be overriden.
 		/// By default, only rules not in a ruleset will be used.
 		/// </summary>
 		/// <param name="context"></param>
 		/// <param name="ruleSets"></param>
-		public static void SetRulesetForClientValidation(HttpContext context, string[] ruleSets) {
-			context.Items[_key] = ruleSets;
-		}
+		public static void SetRulesetForClientValidation(HttpContext context, string[] ruleSets) => context.SetRulesetForClientsideMessages(ruleSets);
 
 		/// <summary>
 		/// Gets the rulesets used to generate clientside validation metadata.
 		/// </summary>
 		/// <param name="context"></param>
 		/// <returns></returns>
-		public static string[] GetRuleSetsForClientValidation(HttpContext context) {
-			// If the httpContext is null (for example, if IHttpContextProvider hasn't been registered) then just assume default ruleset.
-			// This is OK because if we're actually using the attribute, the OnActionExecuting will have caught the fact that the provider is not registered. 
-
-			if (context?.Items != null && context.Items.ContainsKey(_key)) {
-				return context?.Items[_key] as string[] ?? new[] { RulesetValidatorSelector.DefaultRuleSetName };
-
-			}
-			return new[] { RulesetValidatorSelector.DefaultRuleSetName};
-		}
+		public static string[] GetRuleSetsForClientValidation(HttpContext context) => context.GetRuleSetsForClientValidation();
 	}
 }
