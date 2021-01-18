fbc75493fcc6300fd8e84b22527cb1e773a7c0eb
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2021-01-18 13:07:42 +0000

    Missing license header

--- src/FluentValidation/IValidationContext.cs
+++ src/FluentValidation/IValidationContext.cs
@@ -1,28 +1,26 @@
+#region License
+// Copyright (c) .NET Foundation and contributors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// The latest version of this file can be found at https://github.com/FluentValidation/FluentValidation
+#endregion
+
 namespace FluentValidation {
 	using System;
 	using System.Collections.Generic;
 	using Internal;
 	using Results;
-
-	// /// <summary>
-	// /// Defines a validation context.
-	// /// </summary>
-	// public interface ICommonContext {
-	// 	/// <summary>
-	// 	/// The object currently being validated.
-	// 	/// </summary>
-	// 	object InstanceToValidate { get; }
-	//
-	// 	/// <summary>
-	// 	/// The value of the property being validated.
-	// 	/// </summary>
-	// 	object PropertyValue { get; }
-	//
-	// 	/// <summary>
-	// 	/// Parent validation context.
-	// 	/// </summary>
-	// 	ICommonContext ParentContext { get; }
-	// }
 
 	public interface IValidationContext {
 		/// <summary>
