1398c9ba23efba1a714c1c5fe6e07aaf68d2a620
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2021-08-12 12:58:34 +0100

    Update to .net 6 preview 7; remove preview 6 workarounds.

--- src/FluentValidation.Tests.AspNetCore/WebAppFixture.cs
+++ src/FluentValidation.Tests.AspNetCore/WebAppFixture.cs
@@ -10,14 +10,6 @@
 	using Microsoft.Extensions.DependencyInjection;
 
 	public class WebAppFixture : WebApplicationFactory<Startup> {
-		public WebAppFixture() {
-			// using builder.UseContentRoot inside ConfigureWebHost doesn't work
-			// in .net6 anymore as it explicitly checks the testing manifest file instead.
-			// Delete the manifest file to revert to the net5 behaviour.
-			if (File.Exists("MvcTestingAppManifest.json")) {
-				File.Delete("MvcTestingAppManifest.json");
-			}
-		}
 
 		protected override void ConfigureWebHost(IWebHostBuilder builder) {
 			builder.UseContentRoot(".");
