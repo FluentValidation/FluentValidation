29b99ec5bcc0576fbaae4701d551b048af9577a7
Sergei <sergei.valko98@gmail.com>
2022-09-11 14:01:01 +0300

    MemberNameValidatorSelector should make use of PropertyNameResolver when parsing expressions (#2003)

--- src/FluentValidation.Tests/NameResolutionPluggabilityTester.cs
+++ src/FluentValidation.Tests/NameResolutionPluggabilityTester.cs
@@ -26,7 +26,6 @@ public class NameResolutionPluggabilityTester : IDisposable {
 
 		var error = validator.Validate(new Person { Address = new Address() }).Errors.Single();
 		error.PropertyName.ShouldEqual("Address.Country");
-
 	}
 
 	[Fact]
@@ -37,6 +36,36 @@ public class NameResolutionPluggabilityTester : IDisposable {
 				v => v.RuleFor(x => x.Surname).NotNull()
 			};
 			validator.TestValidate(new Person()).ShouldHaveValidationErrorFor(x => x.Surname);
+		}
+		finally {
+			ValidatorOptions.Global.PropertyNameResolver = null;
+		}
+	}
+
+	[Fact]
+	public void ShouldHaveValidationError_Should_support_custom_propertynameresolver_with_include_properties() {
+		try {
+			ValidatorOptions.Global.PropertyNameResolver = (type, prop, expr) => "foo";
+			var validator = new TestValidator() {
+				v => v.RuleFor(x => x.Surname).NotNull()
+			};
+			validator.TestValidate(new Person(), strategy => strategy.IncludeProperties(x => x.Surname)).ShouldHaveValidationErrorFor(x => x.Surname);
+		}
+		finally {
+			ValidatorOptions.Global.PropertyNameResolver = null;
+		}
+	}
+
+	[Fact]
+	public void ShouldHaveValidationError_Should_support_custom_propertynameresolver_with_include_properties_and_nested_properties() {
+		try {
+			ValidatorOptions.Global.PropertyNameResolver = (type, prop, expr) => "foo";
+			var validator = new TestValidator() {
+				v => v.RuleFor(x => x.Address.Line1).NotNull()
+			};
+			validator.TestValidate(new Person {
+				Address = new Address()
+			}, strategy => strategy.IncludeProperties(x => x.Address.Line1)).ShouldHaveValidationErrorFor(x => x.Address.Line1);
 		}
 		finally {
 			ValidatorOptions.Global.PropertyNameResolver = null;
