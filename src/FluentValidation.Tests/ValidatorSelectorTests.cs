029a446b79210e9aa33a4d695c183e74b8f1a715
Jeremy Skinner <90130+JeremySkinner@users.noreply.github.com>
2023-01-23 14:47:36 +0000

    Tweaks to the tests and move them to ValidatorSelectorTests

--- src/FluentValidation.Tests/ValidatorSelectorTests.cs
+++ src/FluentValidation.Tests/ValidatorSelectorTests.cs
@@ -18,8 +18,10 @@
 
 namespace FluentValidation.Tests;
 
+using System.Collections.Generic;
 using Xunit;
 using System.Threading.Tasks;
+using Internal;
 
 public class ValidatorSelectorTests {
 
@@ -161,6 +163,87 @@ public class ValidatorSelectorTests {
 
 		result = validator.Validate(new Person { Age = 1 }, v => v.IncludeProperties("Age"));
 		result.Errors.Count.ShouldEqual(0);
+	}
+
+	[Fact]
+	public void Only_validates_doubly_nested_property() {
+		var person = new Person {
+			Address = new Address {
+				Country = new Country()
+			},
+			Orders = new List<Order> {
+				new() {Amount = 5},
+				new() {ProductName = "Foo"}
+			}
+		};
+
+		var validator = new InlineValidator<Person>();
+		validator.RuleFor(x => x.Address.Country.Name).NotEmpty();
+
+		// ChildRules should not be included. Bug prior to 11.1.1 meant that ChildRules were
+		// incorrectly included for execution.
+		validator.RuleForEach(x => x.Orders).ChildRules(x => {
+			x.RuleFor(y => y.Amount).GreaterThan(6);
+			x.RuleFor(y => y.ProductName).MinimumLength(5);
+		});
+
+		var result = validator.Validate(person, options => options.IncludeProperties("Address.Country.Name"));
+		result.Errors.Count.ShouldEqual(1);
+		result.Errors[0].PropertyName.ShouldEqual("Address.Country.Name");
+		result.Errors[0].ErrorMessage.ShouldEqual("'Address Country Name' must not be empty.");
+	}
+
+	[Fact]
+	public void Only_validates_child_property_for_single_item_in_collection() {
+		var person = new Person {
+			Address = new Address {
+				Country = new Country()
+			},
+			Orders = new List<Order> {
+				new() {Amount = 5},
+				new() {ProductName = "Foo"}
+			}
+		};
+
+		var validator = new InlineValidator<Person>();
+		validator.RuleFor(x => x.Address.Country.Name).NotEmpty();
+		validator.RuleForEach(x => x.Orders).ChildRules(x => {
+			x.RuleFor(y => y.Amount).GreaterThan(6);
+			x.RuleFor(y => y.ProductName).MinimumLength(5);
+		});
+
+		var result = validator.Validate(person, opt => opt.IncludeProperties("Orders[1].Amount"));
+		result.Errors.Count.ShouldEqual(1);
+		result.Errors[0].PropertyName.ShouldEqual("Orders[1].Amount");
+		result.Errors[0].ErrorMessage.ShouldEqual("'Amount' must be greater than '6'.");
+	}
+
+	[Fact]
+	public void Only_validates_single_child_property_of_all_elements_in_collection() {
+		var person = new Person {
+			Address = new Address {
+				Country = new Country()
+			},
+			Orders = new List<Order> {
+				new() {Amount = 5},
+				new() {ProductName = "Foo"},
+				new() {Amount = 10}
+			}
+		};
+
+		var validator = new InlineValidator<Person>();
+		validator.RuleFor(x => x.Address.Country.Name).NotEmpty();
+		validator.RuleForEach(x => x.Orders).ChildRules(x => {
+			x.RuleFor(y => y.Amount).GreaterThan(6);
+			x.RuleFor(y => y.ProductName).MinimumLength(5);
+		});
+
+		var result = validator.Validate(person, opt => opt.IncludeProperties("Orders[].Amount"));
+		result.Errors.Count.ShouldEqual(2);
+		result.Errors[0].PropertyName.ShouldEqual("Orders[0].Amount");
+		result.Errors[0].ErrorMessage.ShouldEqual("'Amount' must be greater than '6'.");
+		result.Errors[1].PropertyName.ShouldEqual("Orders[1].Amount");
+		result.Errors[1].ErrorMessage.ShouldEqual("'Amount' must be greater than '6'.");
 	}
 
 	private class TestObject {
