4140e1f0d9f1d89112dbfa150fc22b935ac5c57d
Jeremy Skinner <90130+JeremySkinner@users.noreply.github.com>
2023-10-04 15:59:27 +0100

    Update EmptyValidator implementation to be consistent with NotEmptyValidator

--- src/FluentValidation/Validators/EmptyValidator.cs
+++ src/FluentValidation/Validators/EmptyValidator.cs
@@ -30,13 +30,20 @@ public class EmptyValidator<T,TProperty> : PropertyValidator<T,TProperty> {
 	public override string Name => "EmptyValidator";
 
 	public override bool IsValid(ValidationContext<T> context, TProperty value) {
-		switch (value) {
-			case null:
-			case string s when string.IsNullOrWhiteSpace(s):
-			case ICollection {Count: 0}:
-			case Array {Length: 0}:
-			case IEnumerable e when !e.GetEnumerator().MoveNext():
-				return true;
+		if (value == null) {
+			return true;
+		}
+
+		if (value is string s) {
+			return string.IsNullOrWhiteSpace(s);
+		}
+
+		if (value is ICollection col) {
+			return col.Count == 0;
+		}
+
+		if (value is IEnumerable e) {
+			return !e.GetEnumerator().MoveNext();
 		}
 
 		return EqualityComparer<TProperty>.Default.Equals(value, default);
