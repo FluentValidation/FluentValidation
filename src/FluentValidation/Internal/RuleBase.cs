5bcab9055d75d2f2d32e3a36d369dfc010f95fd2
Zhannur Akhmetkhanov <63536315+hogwartsdeveloper@users.noreply.github.com>
2024-05-25 16:18:07 +0500

    Add TryGetPropertyValue to IValidationRule (#2124)

--- src/FluentValidation/Internal/RuleBase.cs
+++ src/FluentValidation/Internal/RuleBase.cs
@@ -277,6 +277,16 @@ internal abstract class RuleBase<T, TProperty, TValue> : IValidationRule<T, TVal
 
 	object IValidationRule<T>.GetPropertyValue(T instance) => PropertyFunc(instance);
 
+	bool IValidationRule<T>.TryGetPropertyValue<TProp>(T instance, out TProp value) {
+		value = default;
+
+		var result = PropertyFunc(instance);
+		if (result is not TProp propertyValue) return false;
+
+		value = propertyValue;
+		return true;
+	}
+
 	/// <summary>
 	/// Prepares the <see cref="MessageFormatter"/> of <paramref name="context"/> for an upcoming <see cref="ValidationFailure"/>.
 	/// </summary>
